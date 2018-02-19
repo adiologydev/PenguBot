7f5af4e936d12f05c11aa816a30d2b75da9d2247
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-02-19 23:54:30 +0530

    Added a bunch of Pengu pics + disabled email travis notifications

--- commands/fun/pengu.js
+++ commands/fun/pengu.js
@@ -1,12 +1,12 @@
 const { Command } = require("discord.js-commando");
 const pengus = ["http://i.imgur.com/Urfp335.png",
-    "http://i.imgur.com/gC8v9fp.jpg",
-    "http://i.imgur.com/DZ6YtvT.jpg",
-    "http://i.imgur.com/LdWARAL.jpg",
-    "http://i.imgur.com/7uF0u9Q.jpg",
-    "http://i.imgur.com/0vgVnpr.png",
-    "http://i.imgur.com/1GKlyH2.png",
-    "http://i.imgur.com/jlzSELQ.png"
+    "http://i.imgur.com/gC8v9fp.jpg", "http://i.imgur.com/DZ6YtvT.jpg", "http://i.imgur.com/LdWARAL.jpg",
+    "http://i.imgur.com/7uF0u9Q.jpg", "http://i.imgur.com/0vgVnpr.png", "http://i.imgur.com/1GKlyH2.png",
+    "http://i.imgur.com/jlzSELQ.png", "https://i.imgur.com/Vm98hJq.png", "https://i.imgur.com/RF4JeC8.png",
+    "https://i.imgur.com/Co26qmr.png", "https://i.imgur.com/ixSGy7V.jpg", "https://i.imgur.com/WzsIIzN.png",
+    "https://i.imgur.com/v8oxfHW.jpg", "https://i.imgur.com/RPxPRXV.png", "https://i.imgur.com/UVnwRMk.gif",
+    "https://i.imgur.com/vfKwurE.gif", "https://i.imgur.com/XKukgBG.gif", "https://i.imgur.com/KohkQvr.gif",
+    "https://i.imgur.com/uwdMmng.gif", "https://i.imgur.com/EOln62Q.gif", "https://i.imgur.com/8fcKsh1.gif"
 ];
 module.exports = class PenguCMD extends Command {
 
