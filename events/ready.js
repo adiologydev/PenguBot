0057754925d88991bbb047525b1c5f38a9e672f9
Aditya N. Tripathi <contact@adityatd.me>
2018-04-17 00:01:43 +0530

    Birthday over

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const { PlayerManager } = require("discord.js-lavalink");
 
 module.exports = async (client) => {
     console.log(`Connected and Logged in as ${client.user.tag}, in ${client.guilds.size} guilds`);
-    await client.user.setPresence({ game: { name: "Birthday on 16th April 🎂🎊 | PenguBot.cc", type: 0 } });
+    await client.user.setPresence({ game: { name: "PenguBot.cc | I ❤ You Too | p!help", type: 0 } });
 
     client.player = new PlayerManager(client, client.config.music.nodes, {
         shards: client.shard.count,
