c7da7b65c8d86e0c86ffbe0f5f8633d4149ffe52
QuantumlyTangled <EthoArx@gmail.com>
2018-05-14 20:36:57 +0200

    and toggle pause

--- commands/Music/togglepause.js
+++ commands/Music/togglepause.js
@@ -15,19 +15,13 @@ module.exports = class extends Command {
     }
 
     async run(msg) {
-        const player = this.client.lavalink.get(msg.guild.id);
+        const { music } = msg.guild;
         if (!msg.member.voiceChannel) return msg.sendMessage("<:penguError:435712890884849664> You're currently not in a voice channel.");
-        if (!player) return msg.sendMessage("<:penguError:435712890884849664> There's currently no music playing!");
+        if (!music.playing) return msg.sendMessage("<:penguError:435712890884849664> There's currently no music playing!");
 
         if (!this.client.config.main.patreon || this.client.functions.isPatron(this.client, msg.guild.id === false)) return msg.sendMessage("<:penguError:435712890884849664> ***You need to be in a Patron Guild in order to use this command.***");
 
-        if (player.paused) {
-            player.pause(false);
-            return msg.sendMessage("⏯ | ***PenguBot has Resumed the music!***");
-        } else {
-            player.pause(true);
-            return msg.sendMessage("⏯ | ***PenguBot has Paused the music!***");
-        }
+        return music.smartPause();
     }
 
 };
