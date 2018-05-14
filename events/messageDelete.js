f85494d0869c46d2c0c8ebb8ccd9fb4cfe799547
AdityaTD <adityatripathidelhi@gmail.com>
2018-05-14 17:27:10 +0530

    Bot Checking and Updated Welcome Message

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -5,6 +5,7 @@ module.exports = class extends Event {
 
     async run(message) {
         if (!message.guild) return;
+        if (message.author.bot) return;
         if (message.member.user.id === this.client.user.id) return;
         const log = logger("messages", message.guild, `❌ **Message by ${message.member}** was \`deleted\` in <#${message.channel.id}>`);
         const loggingChannel = message.guild.channels.get(message.guild.configs.loggingChannel);
