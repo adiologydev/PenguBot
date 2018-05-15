4b73e6831c26272bcfabcddb283453deed9b5a85
QuantumlyTangled <EthoArx@gmail.com>
2018-05-15 07:11:46 +0200

    why not?

--- commands/Music/clearqueue.js
+++ commands/Music/clearqueue.js
@@ -17,12 +17,13 @@ module.exports = class extends Command {
 
     async run(msg) {
         const { music } = msg.guild;
+        const size = music.queue.length;
         if (!msg.member.voiceChannel) return msg.sendMessage("<:penguError:435712890884849664> You're currently not in a voice channel.");
         if (!music.playing) return msg.sendMessage("<:penguError:435712890884849664> ***There's currently no music playing!***");
 
         if (await msg.hasAtLeastPermissionLevel(3) || music.voiceChannel.members.size <= 3) {
             music.prune();
-            return msg.sendMessage("<:penguSuccess:435712876506775553> ***Queue cleared.***");
+            return msg.sendMessage(`<:penguSuccess:435712876506775553> ***Queue cleared. The size was ${size} songs.***`);
         } else {
             return msg.sendMessage("<:penguError:435712890884849664> ***There are members in the VC right now, use skip instead!***");
         }
