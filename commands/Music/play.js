b3be27015cae21521f237847b5b7d101656f1737
QuantumlyTangled <EthoArx@gmail.com>
2018-05-17 16:18:25 +0200

    Here you are adi

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,5 +1,8 @@
+const Song = require("../../lib/music/Song.js"); // eslint-disable-line
+const Discord = require("discord.js");
+
 const { Command } = require("klasa");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed } = Discord;
 
 module.exports = class extends Command {
 
@@ -14,6 +17,8 @@ module.exports = class extends Command {
             usage: "<song:string>",
             extendedHelp: "No extended help available."
         });
+
+        this.delayer = time => new Promise(res => setTimeout(() => res(), time));
         this.Music = true;
     }
 
@@ -57,7 +62,7 @@ module.exports = class extends Command {
             return musicInterface.textChannel.send({ embed: await this.stopEmbed() }).then(() => musicInterface.destroy());
         }
 
-        // await this.delayer(500);
+        await this.delayer(500);
 
         musicInterface.play(song.track);
 
@@ -81,6 +86,11 @@ module.exports = class extends Command {
         return musicInterface.textChannel.send({ embed: await this.playEmbed(song) });
     }
 
+    /**
+     * A function to check if it has permission to Connect or Speak and if it cant it will throw a response
+     * @param {Discord.Message} msg The message option from which it gets the guild data
+     * @param {Discord.VoiceChannel} voiceChannel The voicechannel for which it checks if it has permission to connect or speak in
+     */
     resolvePermissions(msg, voiceChannel) {
         const permissions = voiceChannel.permissionsFor(msg.guild.me);
 
@@ -88,40 +98,61 @@ module.exports = class extends Command {
         if (permissions.has("SPEAK") === false) throw "Well, well, well, it seems I can connect, but can't speak. Could you fix that please?";
     }
 
+
+    /**
+     * A embed that is sent when a new song plays
+     * @param {Song} song The song object of which the data is gotten
+     * @returns {MessageEmbed}
+     */
     async playEmbed(song) {
         return new MessageEmbed()
             .setTitle("⏯ | Now Playing - PenguBot")
             .setTimestamp()
             .setFooter("© PenguBot.cc")
             .setColor("#5cb85c")
-            .setDescription([`• **Song:** ${song.trackTitle}`,
+            .setDescription([
+                `• **Song:** ${song.trackTitle}`,
                 `• **Author:** ${song.author}`,
                 `• **Length:** ${song.stream === true ? "Live Stream" : song.trackFriendlyDuration}`,
                 `• **Requested By:** ${song.requester.tag}`,
-                `• **Link:** ${song.trackURL}`]);
+                `• **Link:** ${song.trackURL}`
+            ]);
     }
 
+    /**
+     * A embed that is sent when a song is queued
+     * @param {Song} song The song object of which the data is gotten
+     * @returns {MessageEmbed}
+     */
     async queueEmbed(song) {
         return new MessageEmbed()
             .setTitle("🗒 | Song Queued - PenguBot")
             .setTimestamp()
             .setFooter("© PenguBot.cc")
             .setColor("#eedc2f")
-            .setDescription([`• **Song:** ${song.trackTitle}`,
+            .setDescription([
+                `• **Song:** ${song.trackTitle}`,
                 `• **Author:** ${song.author}`,
                 `• **Length:** ${song.stream === true ? "Live Stream" : song.trackFriendlyDuration}`,
                 `• **Requested By:** ${song.requester.tag}`,
-                `• **Link:** ${song.trackURL}`]);
+                `• **Link:** ${song.trackURL}`
+            ]);
     }
 
+    /**
+     * A embed that is sent when the queue ends
+     * @returns {MessageEmbed}
+     */
     async stopEmbed() {
         return new MessageEmbed()
             .setTitle("⏹ | Queue Finished - PenguBot")
             .setTimestamp()
             .setFooter("© PenguBot.cc")
             .setColor("#d9534f")
-            .setDescription([`• **Party Over:** All the songs from the queue have finished playing. Leaving voice channel.`,
-                `• **Support:** If you enjoyed PenguBot and it's features, please consider becoming a Patron at: https://www.Patreon.com/PenguBot`]);
+            .setDescription([
+                `• **Party Over:** All the songs from the queue have finished playing. Leaving voice channel.`,
+                `• **Support:** If you enjoyed PenguBot and it's features, please consider becoming a Patron at: https://www.Patreon.com/PenguBot`
+            ]);
     }
 
 };
