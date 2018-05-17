be5019b02ac456e5e6a4deebbdbec86245d3791d
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-05-17 22:58:23 +0530

    Leaderboard and New Stats

--- commands/Profiles/leaderboard.js
+++ commands/Profiles/leaderboard.js
@@ -6,26 +6,34 @@ module.exports = class extends Command {
         super(...args, {
             runIn: ["text"],
             aliases: ["lb", "top"],
-            description: msg => msg.language.get("COMMAND_LEADERBOARD_DESCRIPTION")
+            description: msg => msg.language.get("COMMAND_LEADERBOARD_DESCRIPTION"),
+            usage: "[Page:integer]"
         });
     }
 
-    async run(msg) {
-        const message = await msg.send("Fetching guild members...");
-        await msg.guild.members.fetch();
-        const sortedXPs = msg.guild.members.filter(m => !m.user.bot).map(m => ({
-            name: m.user.tag,
-            xp: m.user.configs.xp
-        })).sort((a, b) => b.xp - a.xp);
-        let leaderboardString = "";
-        for (const stats of sortedXPs.slice(0, 10)) leaderboardString += `${sortedXPs.indexOf(stats) + 1}: ${stats.name} - ${stats.xp} XP\n`;
-        return await message.edit(`
-\`\`\`
-${leaderboardString}
------------------------------
-Your Placement
-${sortedXPs.indexOf(sortedXPs.find(x => x.name === msg.author.tag)) + 1}: ${msg.author.tag} - ${msg.author.configs.xp} XP
-\`\`\``);
+    async run(msg, [Page]) {
+        const users = await this.client.providers.get("rethinkdb").getAll("users").then(res => res.sort((a, b) => b.xp - a.xp));
+        const userPos = users.filter(a => this.client.users.get(a.id));
+
+        const leaderboard = [];
+        const totalPages = Math.round(userPos.length / 10);
+
+        const index = Page ? Page -= 1 : 0;
+
+        if ((index > totalPages && !totalPages) || (totalPages && index + 1 > totalPages)) return msg.channel.send(`There are only **${totalPages || 1}** page(s) in the leaderboard.`);
+
+        leaderboard.push("=== PenguBot Global Leaderboard ===\n");
+        const pos = userPos.findIndex(i => i.id === msg.author.id);
+
+        userPos.slice(index * 10, (index + 1) * 10)
+            .map(user => ({ xp: user.xp, user: user.id }))
+            .forEach((newMap, position) =>
+                leaderboard.push(` • ${((index * 10) + (position + 1)).toString().padStart(2, " ")} | ${this.client.users.get(newMap.user).tag.padEnd(30, " ")}::  ${newMap.xp.toLocaleString()} XPs`)
+            );
+
+        leaderboard.push(`\n • ${pos !== -1 ? pos + 1 : "???"} | ${msg.author.tag.padEnd(30, " ")}::  ${this.client.users.get(msg.author.id).configs.xp.toLocaleString()} XPs`);
+        leaderboard.push("--------------------------------------------------");
+        return msg.channel.send(`${leaderboard.join("\n")}\n Page ${index + 1} / ${totalPages || 1} - ${userPos.length} Total Users`, { code: "asciidoc" });
     }
 
 };
