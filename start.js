77371429ac00b6a9d83806cd348a6449180e672a
Jacz <jacz@jaczaus.me>
2018-05-29 07:41:10 +1000

    Update start file

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require("discord.js");
 const { main } = require("./config.json");
-const manager = new ShardingManager(`${__dirname}/app.js`, { totalShards: main.shards, token: main.token, respawn: true });
+const manager = new ShardingManager(`./app.js`, { totalShards: main.shards, token: main.token, respawn: true });
 
 manager.spawn();
 manager.on("launch", shard => console.log(`💠- Shard ${shard.id} is now online`));
