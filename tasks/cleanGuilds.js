f37f5f08c83eba1cc33deff1a889e9f4eb5a2b85
Jacz <jacz@jaczaus.me>
2018-06-01 11:45:25 +1000

    Task: Update(cleanGuilds): Improvments

--- tasks/cleanGuilds.js
+++ tasks/cleanGuilds.js
@@ -3,15 +3,13 @@ const { Task } = require("klasa");
 module.exports = class extends Task {
 
     async run() {
-        if (this.client.config.main.patreon) {
-            const { guilds } = this.client;
-            for (const g of guilds) {
-                if (!this.client.configs.pGuilds.includes(g.id)) await g.leave();
-            }
+        for (const guild of this.client.guilds) {
+            if (!this.client.configs.pGuilds.includes(guild.id)) await guild.leave();
         }
     }
 
     async init() {
+        if (!this.client.config.main.patreon) return this.disable();
         if (!this.client.configs.schedules.some(schedule => schedule.taskName === this.name)) {
             await this.client.schedule.create("cleanGuilds", "0 0 * * *");
         }
