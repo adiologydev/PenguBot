3a519e91f007c7098910a0a4c57890d55f146fcb
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-06-05 00:35:22 +0530

    Does this help?

--- inhibitors/migrationDisable.js
+++ inhibitors/migrationDisable.js
@@ -3,7 +3,7 @@ const { Inhibitor } = require("klasa");
 module.exports = class extends Inhibitor {
 
     constructor(...args) {
-        super(...args, { spamProtection: true });
+        super(...args, { spamProtection: false });
     }
 
     async run(msg) {
