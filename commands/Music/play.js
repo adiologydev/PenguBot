62e95b9809f3606a109f0bc966acf30ff3680fce
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-06-05 17:38:10 +0530

    More More

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -20,7 +20,10 @@ module.exports = class extends Command {
     /* eslint-disable complexity */
     async run(msg, [song]) {
         const url = encodeURIComponent(song);
-        if (!msg.member.voiceChannel || !msg.member) return msg.sendMessage("<:penguError:435712890884849664> ***You're currently not in a Voice Channel, please join one to use this command.***");
+        await msg.guild.members.fetch(msg.author.id).catch(() => {
+            throw "I tripped on a wire! *Ouch!* It hurts but I'll recover, try again later.";
+        });
+        if (!msg.member.voiceChannel) return msg.sendMessage("<:penguError:435712890884849664> ***You're currently not in a Voice Channel, please join one to use this command.***");
         if (this.client.functions.validURL(song)) {
             const playlist = /(\?|\&)list=(.*)/.exec(song); // eslint-disable-line
             const soundCloud = /https:\/\/soundcloud\.com\/.*/.exec(url); // eslint-disable-line
