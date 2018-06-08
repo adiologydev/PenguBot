c48b7ffc4ffe59db1cef6d5f183dfbcfb5838acd
Jacz <jacz@jaczaus.me>
2018-06-08 13:02:31 +1000

    Fix stats command

--- commands/General/Chat Bot Info/stats.js
+++ commands/General/Chat Bot Info/stats.js
@@ -12,32 +12,30 @@ module.exports = class extends Command {
     async run(msg) {
         let [users, guilds, channels, memory, vc] = [0, 0, 0, 0, 0];
 
-        if (this.client.shard) {
-            const results = await this.client.shard.broadcastEval(`[this.users.reduce((prev, val) => val.memberCount + prev, 0), this.guilds.size, this.channels.size, (process.memoryUsage().heapUsed / 1024 / 1024), this.lavalink.size]`);
-            for (const result of results) {
-                users += result[0];
-                guilds += result[1];
-                channels += result[2];
-                memory += result[3];
-                vc += result[4];
-            }
+        const results = await this.client.shard.broadcastEval(`[this.guilds.reduce((prev, val) => val.memberCount + prev, 0), this.guilds.size, this.channels.size, (process.memoryUsage().heapUsed / 1024 / 1024), this.lavalink.size]`);
+        for (const result of results) {
+            users += result[0];
+            guilds += result[1];
+            channels += result[2];
+            memory += result[3];
+            vc += result[4];
         }
 
         const embed = new MessageEmbed()
             .setColor("RANDOM")
             .setTimestamp()
             .setThumbnail("https://i.imgur.com/HE0ZOSA.png")
-            .addField("Memory Usage", `${(memory || process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+            .addField("Memory Usage", `${memory} MB`, true)
             .addField("Uptime", Duration.toNow(Date.now() - (process.uptime() * 1000)), true)
-            .addField("Users", (users || this.client.users.size).toLocaleString(), true)
-            .addField("Guilds", (guilds || this.client.guilds.size).toLocaleString(), true)
-            .addField("Channels", (channels || this.client.channels.size).toLocaleString(), true)
-            .addField("Voice Streams", (vc || this.client.lavalink.size).toLocaleString(), true)
+            .addField("Users", users.toLocaleString(), true)
+            .addField("Guilds", guilds.toLocaleString(), true)
+            .addField("Channels", channels.toLocaleString(), true)
+            .addField("Voice Streams", vc.toLocaleString(), true)
             .addField("Discord Version", discordVersion, true)
-            .addField("Shards", `${((msg.guild ? msg.guild.shardID : msg.channel.shardID) || this.client.options.shardId) + 1} / ${this.client.options.shardCount}`, true)
+            .addField("Shards", `${this.client.shard.id + 1} / ${this.client.shard.count}`, true)
             .setAuthor("PenguBot - Statistics", this.client.user.displayAvatarURL(), "https://www.pengubot.com");
 
-        return msg.sendMessage({ embed: embed });
+        return msg.sendMessage({ embed });
     }
 
 };
