f775d9d152151be2d9e2de15b719601d26cd6706
Jacz <jacz@jaczaus.me>
2018-06-08 11:18:02 +1000

    Improvments

--- tasks/cleanGuilds.js
+++ tasks/cleanGuilds.js
@@ -2,15 +2,12 @@ const { Task } = require("klasa");
 
 module.exports = class extends Task {
 
-    async run() {
-        if (this.client.user.id === "303181184718995457") return;
-        for (const guild of this.client.guilds.values()) {
-            if (!this.client.configs.pGuilds.includes(guild.id)) await guild.leave();
-        }
+    run() {
+        return Promise.all(this.client.guilds.filter(g => !this.client.configs.pGuilds.includes(g.id)).map(g => g.leave()));
     }
 
     async init() {
-        if (!this.client.config.main.patreon) return this.disable();
+        if (this.client.user.id === "303181184718995457" || !this.client.config.main.patreon) return this.disable();
         if (!this.client.configs.schedules.some(schedule => schedule.taskName === this.name)) {
             await this.client.schedule.create("cleanGuilds", "0 0 * * *");
         }
