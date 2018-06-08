01b3d59a134b0b52e99ccf62d5da494677a21f5f
Jacz <jacz@jaczaus.me>
2018-06-08 13:03:24 +1000

    use guilds not users

--- commands/General/Chat Bot Info/shards.js
+++ commands/General/Chat Bot Info/shards.js
@@ -12,7 +12,7 @@ module.exports = class extends Command {
     }
 
     async run(msg) {
-        const evalstr = `[this.shard.id, this.guilds.size, this.channels.size, this.users.size, (process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2), this.lavalink.size, this.uptime]`;
+        const evalstr = `[this.shard.id, this.guilds.size, this.channels.size, this.guilds.reduce((prev, val) => val.memberCount + prev, 0), (process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2), this.lavalink.size, this.uptime]`;
         const result = await this.client.shard.broadcastEval(evalstr);
         return msg.sendMessage(`= Shards Information =\n\n${result.map(r => `${r[0]} : G ${r[1]}, C ${r[2]}, U ${r[3]}, M ${r[4]}, VC ${r[5]}, UP: ${formatUptime(r[6])}`).join("\n")}`, { code: "prolog" });
     }
