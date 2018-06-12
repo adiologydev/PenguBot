d01e72528942f51ca9a28c038871d34cf9e958df
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-06-12 19:56:28 +0530

    Will this avoid error logs?

--- tasks/reminder.js
+++ tasks/reminder.js
@@ -5,8 +5,8 @@ module.exports = class extends Task {
     async run({ channel, user, text }) {
         const _channel = this.client.channels.get(channel);
         const _user = await this.client.users.fetch(user).catch(() => null);
-        if (_user && _user.send) await _user.send(`⏰ **Reminder:** ${text}`);
-        else await _channel.send(`📘 | ${_user}, **Reminder:** ${text}`);
+        if (_user && _user.send) await _user.send(`⏰ **Reminder:** ${text}`).catch(() => null);
+        else await _channel.send(`📘 | ${_user}, **Reminder:** ${text}`).catch(() => null);
     }
 
 };
