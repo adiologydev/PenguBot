6bec2ff36cb8c6f6e0ea7e324d14fa376fd11330
QuantumlyTangled <EthoArx@gmail.com>
2018-06-23 18:53:20 +0200

    Forgot to save

--- tasks/health.js
+++ tasks/health.js
@@ -3,8 +3,8 @@ const { Task } = require("klasa");
 module.exports = class extends Task {
 
     async run() {
-        this.client.health.cmd.cmdCount.shift();
-        this.client.health.cmd.cmdCount.push(this.client.health.commands.temp);
+        this.client.health.commands.cmdCount.shift();
+        this.client.health.commands.cmdCount.push(this.client.health.commands.temp);
         this.client.health.commands.temp = {
             count: 0,
             ran: {}
