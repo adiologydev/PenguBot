8334d56209b5304dd0deadab4bc8cb5583747caf
Jacz <jacz@jaczaus.me>
2018-06-25 00:23:16 +1000

    Fix voiceStateUpdate

--- rawEvents/VOICE_STATE_UPDATE.js
+++ rawEvents/VOICE_STATE_UPDATE.js
@@ -1,10 +1,7 @@
 const RawEvent = require("../structures/RawEvent");
+const { util: { sleep } } = require("klasa");
 
 class VoiceStateUpdate extends RawEvent {
-
-    constructor(...args) {
-        super(...args, { name: "VOICE_STATE_UPDATE" });
-    }
 
     async run(data) {
         const guild = this.client.guilds.get(data.guild_id);
@@ -18,7 +15,26 @@ class VoiceStateUpdate extends RawEvent {
 
         guild.voiceStates.set(member.user.id, data);
 
-        this.client.emit("voiceStateUpdate", oldMember, member);
+        await this.voiceStateUpdate(oldMember, member);
+    }
+
+    async voiceStateUpdate(oldMem, newMem) {
+        if (this.client.config.main.patreon) return;
+        await sleep(10000);
+        if (!newMem.guild.me) await newMem.guild.members.fetch(this.client.user.id);
+        const queue = this.client.queue.get(newMem.guild.id);
+        if (!queue) return;
+        await sleep(300000);
+        if (!newMem.guild.me) await newMem.guild.members.fetch(this.client.user.id);
+        if (oldMem.voiceChannel === oldMem.guild.me.voiceChannel && newMem.voiceChannel !== newMem.guild.me.voiceChannel && newMem.guild.me.voiceChannel.members.size === 1) {
+            try {
+                await this.client.lavalink.leave(newMem.guild.id);
+                if (queue.tc) await queue.tc.send("<:penguError:435712890884849664> ***No one left in Voice Channel, leaving...***");
+                return this.client.queue.delete(newMem.guild.id);
+            } catch (error) {
+                this.client.console.error(error);
+            }
+        }
     }
 
 }
