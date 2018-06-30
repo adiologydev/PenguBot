f272a713c72e1db7586e037b5958cf6465505209
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-06-30 20:01:35 +0530

    Wellp

--- utils/functions.js
+++ utils/functions.js
@@ -29,7 +29,7 @@ class Util {
             snekfetch.post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
                 .set("Authorization", config.keys.dbpw).send(stats),
             snekfetch.post(`https://listcord.com/api/bot/${client.user.id}/guilds`)
-                .set("Authorization", config.keys.listcord).send({ guilds: client.guilds.size, shard: client.shard.id }),
+                .set("token", config.keys.listcord).send({ guilds: client.guilds.size, shard: client.shard.id }),
             snekfetch.post(`https://botsfordiscord.com/api/v1/bots/${client.user.id}`)
                 .set("Authorization", config.keys.b4d).send({ server_count: allGuilds.reduce((prev, val) => prev + val, 0) })
         ]);
