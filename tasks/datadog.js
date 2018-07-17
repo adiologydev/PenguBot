afedd1059b536de8fd03fbbda8e7bcfc5daf77a2
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-07-18 00:19:10 +0530

    fix: it was making cleaner's schedule, not datadog

--- tasks/datadog.js
+++ tasks/datadog.js
@@ -7,8 +7,8 @@ module.exports = class extends Task {
     async run() {
         const allGuilds = await this.client.shard.fetchClientValues("guilds.size");
         const allVc = await this.client.shard.fetchClientValues("lavalink.size");
-        dogstats.set("pengubots.cmdscounter", this.client.configs.counter.total);
-        dogstats.set("pengubots.voicestreams", allVc.reduce((prev, val) => prev + val, 0));
+        dogstats.gauge("pengubot.cmdscounter", this.client.configs.counter.total);
+        dogstats.gauge("pengubot.vc", allVc.reduce((prev, val) => prev + val, 0));
         return dogstats.gauge("pengubot.guilds", allGuilds.reduce((prev, val) => prev + val, 0));
     }
 
@@ -16,7 +16,7 @@ module.exports = class extends Task {
     async init() {
         if (this.client.user.id !== "303181184718995457" && this.client.shard.id !== 0) return this.disable();
         if (!this.client.configs.schedules.some(schedule => schedule.taskName === this.name)) {
-            await this.client.schedule.create("cleaner", "*/1 * * * *");
+            await this.client.schedule.create("datadog", "*/1 * * * *");
         }
     }
 
