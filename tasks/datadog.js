32befacdc0b29d989506cb70e8f3a11ba679d6fb
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-07-17 22:32:17 +0530

    fix: disabled on wrong client

--- tasks/datadog.js
+++ tasks/datadog.js
@@ -2,7 +2,7 @@ const { Task } = require("klasa");
 const { StatsD } = require("node-dogstatsd");
 const dogstats = new StatsD("localhost", 8125);
 
-module.exports = class MemorySweeper extends Task {
+module.exports = class extends Task {
 
     async run() {
         const allGuilds = await this.client.shard.fetchClientValues("guilds.size");
@@ -14,7 +14,7 @@ module.exports = class MemorySweeper extends Task {
 
     // Init
     async init() {
-        if (this.client.user.id === "303181184718995457" || !this.client.config.main.patreon) return this.disable();
+        if (this.client.user.id !== "303181184718995457") return this.disable();
         if (!this.client.configs.schedules.some(schedule => schedule.taskName === this.name)) {
             await this.client.schedule.create("cleaner", "*/1 * * * *");
         }
