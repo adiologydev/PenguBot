4659088ee74ad3f0372e0ab262408a76e5fc9b26
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-08-01 19:44:00 +0530

    fix: positions being inaccurate

--- commands/Profiles/profile.js
+++ commands/Profiles/profile.js
@@ -39,7 +39,7 @@ module.exports = class extends Command {
 
         const query = await r.db("pengubot").table("users").orderBy({ index: r.desc("xp") }).pluck("id", "xp").limit(10000).offsetsOf(r.row("id").eq(user.id))
             .run();
-        const pos = query ? `#${query + 1}` : "More Than 10,000";
+        const pos = query ? `#${Number(query) + 1}` : "More Than 10,000";
 
         const bgName = user.configs.profilebg;
         const bgImg = await fs.readFile(`${process.cwd()}/assets/profiles/bg/${bgName}.png`);
