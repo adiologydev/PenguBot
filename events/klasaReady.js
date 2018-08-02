dbd6f01d1098d7a86b6a664d1875ebc77622bcb1
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-08-02 23:11:48 +0530

    add: voting rewards

--- events/klasaReady.js
+++ events/klasaReady.js
@@ -1,5 +1,6 @@
 const { Event } = require("klasa");
 const MusicClient = require("../lib/structures/LavalinkClient");
+const DBL = require("dblapi.js");
 
 module.exports = class extends Event {
 
@@ -7,6 +8,16 @@ module.exports = class extends Event {
         // Setup lavalink
         this.client.lavalink = new MusicClient(this.client, this.client.config.nodes, { shards: this.client.shard.count ? this.client.shard.count : 1 });
         if (this.client.config.main.status) this.client.whStatus.send(`✅ **ONLINE:** Shard \`${this.client.shard.id}\` is now online.`);
+
+        // Setup DBL Webhook
+        if (this.client.shard.id === 0) {
+            const dbl = new DBL(this.client.config.keys.dbl, {
+                webhookPort: 2425,
+                webhookAuth: this.client.config.keys.dbl
+            });
+            dbl.webhook.on("vote", vote =>
+                this.client.tasks.get("voteRewards").run(vote));
+        }
     }
 
 };
