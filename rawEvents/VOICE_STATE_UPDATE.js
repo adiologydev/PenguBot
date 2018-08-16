d8a4090157674ffe7517609d48e60e8d9e8040c2
Jacz <jacz@jaczaus.me>
2018-08-17 09:59:27 +1000

    fix dumb

--- rawEvents/VOICE_STATE_UPDATE.js
+++ rawEvents/VOICE_STATE_UPDATE.js
@@ -1,5 +1,4 @@
-const RawEvent = require("../lib/structures/RawEvent");
-const { VoiceState } = require('discord.js');
+const { RawEvent } = require("../index.js");
 
 class VoiceStateUpdate extends RawEvent {
 
@@ -10,16 +9,10 @@ class VoiceStateUpdate extends RawEvent {
     async run(data) {
         const guild = this.client.guilds.get(data.guild_id);
         if (!guild) return;
-        const member = await guild.members.fetch(data.user_id).catch(() => null);
-        if (!member) return;
 
-        // Patch d.js
-        const oldMember = member._clone();
-        oldMember._frozenVoiceState = oldMember.voiceState;
+        await guild.members.fetch(data.user_id).catch(() => null);
 
-        const entry = new VoiceState(guild.id, data);
-        
-        guild.voiceStates.set(member.user.id, entry);
+        guild.voiceStates.add(data);
     }
 
 }
