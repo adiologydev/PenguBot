ccaf4e53b9e5a6ebe4a03da534a020208ed1d26f
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-08-17 19:10:07 +0530

    fix: just making sure it doesn't overwrite non-cached users snowflakes

--- tasks/voteRewards.js
+++ tasks/voteRewards.js
@@ -3,7 +3,10 @@ const { Task } = require("klasa");
 module.exports = class extends Task {
 
     async run(vote) {
-        const user = await this.client.users.fetch(vote.user);
+        const user = await this.client.users.fetch(vote.user).then(() => null);
+        if (!user) return;
+
+        await user.settings.sync(true);
         let amount = this.client.functions.randomNumber(100, 200);
         if (vote.isWeekend) amount *= 2;
         if (user && user.send) user.send(`<:penguSuccess:435712876506775553> ***Thank you for Upvoting PenguBot, you've recieved ❄ \`${amount}\` Snowflakes as a bonus! You can do this again after 12 hours at <https://www.pengubot.com/upvote>. You can use these Snowflakes to play games, give them to other people, buy profile backgrounds or save up for upcoming features!***\n\n**Tip: **\`Voting on Weekends will give you double reward than usual so don't forget to upvote then as well!\``);
