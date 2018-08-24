9bd3b99472d5cd7754d11d3ef978798f62a57206
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-08-24 19:01:37 +0530

    add: Shard.js extension for cluster instead of child_process

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const { ShardingManager } = require("discord.js");
 const { main } = require("./config.json");
 
+require("./lib/extensions/Shard");
+
 const manager = new ShardingManager(`./app.js`, { totalShards: main.shards, token: main.token, respawn: true });
 
 manager.spawn(manager.totalShards, 5500, false);
