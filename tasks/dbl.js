9e2f5831ffcd82be93e7fb2fcfdacc490d5d7e7e
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-08-26 14:49:15 +0530

    fix: dbl stats posting

--- tasks/dbl.js
+++ tasks/dbl.js
@@ -14,7 +14,7 @@ module.exports = class extends Task {
             snekfetch.post(`https://bots.discord.pw/api/bots/${this.client.user.id}/stats`)
                 .set("Authorization", this.client.config.keys.dbpw).send(stats),
             snekfetch.post(`https://discordbotlist.com/api/bots/${this.client.user.id}/stats`)
-                .set("Authorization", this.client.config.keys.ogdbl).send({ guilds: this.client.guilds.size, users: this.client.guilds.reduce((prev, val) => val.memberCount + prev, 0), shard_id: this.client.shard.id, voice_connections: this.client.lavalink.size }),
+                .set("Authorization", `Bot ${this.client.config.keys.ogdbl}`).send({ guilds: this.client.guilds.size, users: this.client.guilds.reduce((prev, val) => val.memberCount + prev, 0), shard_id: this.client.shard.id, voice_connections: this.client.lavalink.size }),
             snekfetch.post(`https://botsfordiscord.com/api/v1/bots/${this.client.user.id}`)
                 .set("Authorization", this.client.config.keys.b4d).send({ server_count: allGuilds.reduce((prev, val) => prev + val, 0) }),
             snekfetch.post(`https://discordbots.group/api/bot/${this.client.user.id}`)
