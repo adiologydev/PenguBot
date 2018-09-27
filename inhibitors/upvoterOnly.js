c6866bcc3339381f3adcb59f83694d7f99de58f1
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-09-27 20:00:07 +0530

    update: upvote message

--- inhibitors/upvoterOnly.js
+++ inhibitors/upvoterOnly.js
@@ -9,7 +9,7 @@ module.exports = class extends Inhibitor {
     async run(msg, cmd) {
         if (!cmd.upvoteOnly) return;
         if (this.client.funcs.isUpvoter(msg.author) || this.client.config.main.patreon) return;
-        throw `${this.client.emotes.cross} ***${msg.language.get("CMD_UPVOTE_ONLY")}***`;
+        throw `🔒 ***${msg.language.get("CMD_UPVOTE_ONLY")}***`;
     }
 
 };
