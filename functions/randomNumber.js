49ac8529cedd4d9320aee4fe449bbbeaa7c9b14f
Santosh <bsantosh909@gmail.com>
2018-10-02 10:33:42 +0545

    Update randomNumber.js

--- functions/randomNumber.js
+++ functions/randomNumber.js
@@ -3,6 +3,9 @@ const { Function } = require("klasa-functions");
 module.exports = class extends Function {
 
     run(min, max) {
+        if (typeof min !== 'number') throw 'Minimun value must be a number';
+        if (typeof max !== 'number') throw 'Maximum value must be a number';
+        if (min >= max) throw 'Minimun must be less than Maximum to determine the range';
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min)) + min;
