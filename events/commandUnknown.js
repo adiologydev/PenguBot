45eaaffbeebda6924f5d6e66a8197d6e5f312bc5
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-10-17 18:01:43 +0530

    fix: commands not getting executed

--- events/commandUnknown.js
+++ events/commandUnknown.js
@@ -5,7 +5,7 @@ module.exports = class extends Event {
 
     async run(msg, command, prefixLength) {
         if (!msg.guild || !msg.guildSettings.customcmds.cmds.length) return;
-        const customCommand = msg.guildSettings.customcmds.cmds.find(c => c.name === command);
+        const customCommand = msg.guildSettings.customcmds.cmds.find(c => c.name.match(new RegExp(command, "i")));
         if (!customCommand) return;
         const args = msg.content.slice(prefixLength).trim().split(" ").slice(1);
         const parsed = await this.parser.parse(customCommand.content, {
