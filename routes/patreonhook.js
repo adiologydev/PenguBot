80ae44f2c263cc573449518dd71c319407d66755
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-10-22 16:51:35 +0530

    fix: patreon hook

--- routes/patreonhook.js
+++ routes/patreonhook.js
@@ -26,22 +26,27 @@ module.exports = class extends Route {
 
         if (body.data.relationships.currently_entitled_rewards.data.length > 0) {
             const me = await this.client.users.fetch("136549806079344640").catch(() => null);
-            await user.settings.update(["patreon.paying", true], ["patreon.tokens", user.settings.patreon.tokens + body.data.attributes.pledge_amount_cents], ["patreon.pledged", user.settings.patreon.pledged + body.data.attributes.pledge_amount_cents])
+            await user.settings.update([["patreon.paying", true], ["patreon.tokens", user.settings.patreon.tokens + body.data.attributes.pledge_amount_cents], ["patreon.pledged", user.settings.patreon.pledged + body.data.attributes.pledge_amount_cents]])
                 .catch(async e => {
-                    await me.send(`<:patreon:502495572721270791> ***User Update Error:*** ${patron.attributes.email}\n\n**Error:**\n${e}`);
+                    await me.send(`<:patreon:502495572721270791> ***User Update Error:*** \`${patron.attributes.email}\` | User: \`${user.id}\`\n\n**Error:**\n${e}`);
                     return response.end(421);
                 });
-            await this.client.settings.update("patrons.users", user)
+            await this.client.settings.update("patrons.users", user, { action: "add" })
                 .catch(async e => {
-                    await me.send(`<:patreon:502495572721270791> ***Client Update Error:*** ${patron.attributes.email}\n\n**Error:**\n${e}`);
+                    await me.send(`<:patreon:502495572721270791> ***Client Update Error:*** \`${patron.attributes.email}\` | User: \`${user.id}\`\n\n**Error:**\n${e}`);
                     return response.end(421);
                 });
-            return me.send(`<:patreon:502495572721270791> ***Gave Rewards To: \`${patron.attributes.email}\` | User: \`${user.id}\`***`);
+            return me.send(`<:patreon:502495572721270791> ***Created Pledge: \`${patron.attributes.email}\` | User: \`${user.id}\`***`);
         } else {
             const me = await this.client.users.fetch("136549806079344640").catch(() => null);
             await user.settings.update("patreon.paying", false)
                 .catch(async e => {
                     await me.send(`<:patreon:502495572721270791> ***User Update Error:*** ${patron.attributes.email}\n\n**Error:**\n${e}`);
+                    return response.end(421);
+                });
+            await this.client.settings.update("patrons.users", user, { action: "remove" })
+                .catch(async e => {
+                    await me.send(`<:patreon:502495572721270791> ***Client Update Error:*** \`${patron.attributes.email}\` | User: \`${user.id}\`\n\n**Error:**\n${e}`);
                     return response.end(421);
                 });
             me.send(`<:patreon:502495572721270791> ***Cancelled Pledge: \`${patron.attributes.email}\` | User: \`${user.id}\`***`);
