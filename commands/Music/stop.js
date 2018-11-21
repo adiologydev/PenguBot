35796cd50d04c9a7beac3c9f678a7d163278cc85
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-11-21 20:42:47 +0530

    fix: some stuff

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -5,7 +5,7 @@ module.exports = class extends MusicCommand {
     constructor(...args) {
         super(...args, {
             requireDJ: true,
-            requireMusic: true,
+            requireMusic: false,
             cooldown: 8,
             aliases: ["forceleave", "leave", "stopmusic", "musicstop", "stop"],
             requiredPermissions: ["USE_EXTERNAL_EMOJIS"],
