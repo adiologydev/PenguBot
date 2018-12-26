8e9c781083e67db59a6fc4501e2d963e947e7f64
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2018-12-26 19:24:30 +0530

    eslint: nowplaying

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -6,7 +6,7 @@ module.exports = class extends MusicCommand {
     constructor(...args) {
         super(...args, {
             cooldown: 8,
-            aliases: ["np", "currentsong", "song", "canzone", "chanson", "canción", "lied" ],
+            aliases: ["np", "currentsong", "song", "canzone", "chanson", "canción", "lied"],
             requiredPermissions: ["USE_EXTERNAL_EMOJIS"],
             description: language => language.get("COMMAND_NOWPLAYING_DESCRIPTION"),
             extendedHelp: "No extended help available."
