f37e2e0f5abe9b6c33edc99d86c02beedad984ed
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2019-02-08 23:10:34 +0530

    add: view original message to starboard

--- commands/Management/Starboard/starboard.js
+++ commands/Management/Starboard/starboard.js
@@ -23,6 +23,8 @@ module.exports = class extends Command {
         const fetch = await starChannel.messages.fetch({ limit: 100 });
         const starMsg = fetch.find(m => m.embeds[0] && m.embeds[0].footer && m.embeds[0].footer.text.startsWith("⭐") && m.embeds[0].footer.text.endsWith(Message.id));
 
+        const jumpString = `[► View The Original Message](https://discordapp.com/channels/${Message.guild.id}/${Message.channel.id}/${Message.id})\n`;
+
         if (starMsg) {
             const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(starMsg.embeds[0].footer.text); // eslint-disable-line
             const starEmbed = starMsg.embeds[0];
@@ -30,10 +32,11 @@ module.exports = class extends Command {
             const embed = new MessageEmbed()
                 .setColor(starEmbed.color)
                 .setAuthor(`${Message.author.tag} in #${Message.channel.name}`, Message.author.displayAvatarURL())
-                .setTimestamp()
+                .setTimestamp(new Date(Message.createdTimestamp))
                 .setFooter(`⭐ ${Message.reactions.get("⭐").count} | ${msg.id}`);
             if (image) embed.setImage(image);
-            if (starEmbed.description) embed.setDescription(starEmbed.description);
+            if (starEmbed.description) embed.setDescription(`${jumpString}${starEmbed.description}`);
+            else embed.setDescription(jumpString);
             const oldMsg = await starChannel.messages.fetch(starMsg.id);
             await oldMsg.edit({ embed });
         } else {
@@ -43,10 +46,10 @@ module.exports = class extends Command {
             const embed = new MessageEmbed()
                 .setColor(15844367)
                 .setAuthor(`${Message.author.tag} in #${Message.channel.name}`, Message.author.displayAvatarURL())
-                .setTimestamp(new Date())
+                .setTimestamp(new Date(Message.createdTimestamp))
                 .setFooter(`⭐ ${Message.reactions.get("⭐").count} | ${Message.id}`);
             if (image) embed.setImage(image);
-            if (Message.content) embed.setDescription(Message.content);
+            if (msg.content) embed.setDescription(`${jumpString}${Message.content}`);
             await starChannel.send({ embed });
         }
     }
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -16,16 +16,19 @@ module.exports = class extends Event {
         const fetch = await starChannel.messages.fetch({ limit: 100 });
         const starMsg = fetch.find(m => m.embeds[0] && m.embeds[0].footer && m.embeds[0].footer.text.startsWith("⭐") && m.embeds[0].footer.text.endsWith(msg.id));
 
+        const jumpString = `[► View The Original Message](https://discordapp.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id})\n`;
+
         if (starMsg) {
             const starEmbed = starMsg.embeds[0];
             const image = msg.attachments.size > 0 ? await this.checkAttachments(msg.attachments.array()[0].url) : null;
             const embed = new MessageEmbed()
                 .setColor(starEmbed.color)
                 .setAuthor(`${msg.author.tag} in #${msg.channel.name}`, msg.author.displayAvatarURL())
-                .setTimestamp()
+                .setTimestamp(new Date(msg.createdTimestamp))
                 .setFooter(`⭐ ${msg.reactions.get("⭐") ? msg.reactions.get("⭐").count : 0} | ${msg.id}`);
             if (image) embed.setImage(image);
-            if (starEmbed.description) embed.setDescription(starEmbed.description);
+            if (msg.content) embed.setDescription(`${jumpString}${msg.content}`);
+            else embed.setDescription(jumpString);
             const oldMsg = await starChannel.messages.fetch(starMsg.id);
             if (oldMsg.author.id !== this.client.user.id) return;
             if (!msg.reactions.get("⭐")) return oldMsg.delete();
