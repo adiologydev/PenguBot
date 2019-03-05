67af9c8aaff35f366637bc232b505835ae625eb7
Jacz <jacz@jaczaus.me>
2019-03-05 22:13:04 +1100

    Human linter

--- start.js
+++ start.js
@@ -6,6 +6,4 @@ require("./src/lib/extensions/Shard");
 const manager = new ShardingManager(`./src/PenguBot.js`, { totalShards: main.shards, token: main.token, respawn: true });
 
 manager.spawn(manager.totalShards, 5500, false);
-manager.on("shardCreate", shard => {
-    console.log(`💠- Shard ${shard.id} is now online`);
-});
+manager.on("shardCreate", shard => console.log(`💠- Shard ${shard.id} is now online`));
