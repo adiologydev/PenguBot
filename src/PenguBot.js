3bcb6e120947385c8f2687c6346106e6480a6abf
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2019-03-23 01:24:33 +0530

    add: raven

--- src/PenguBot.js
+++ src/PenguBot.js
@@ -1,9 +1,18 @@
 const { BaseCluster } = require("kurasuta");
+const Raven = require("raven");
+const { main } = require("./config.json");
 
 module.exports = class extends BaseCluster {
 
     launch() {
-        this.client.login(this.manager.token);
+        Raven.context(() => {
+            this.client.login(main.token);
+        });
+
+        process.on("uncaughtException", err => {
+            console.error(`uncaughtException:\n${err.stack}`);
+            Raven.captureException(err);
+        });
     }
 
 };
