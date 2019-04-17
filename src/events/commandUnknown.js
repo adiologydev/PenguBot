29dee65e39f10975d14cdd7e5da1d569060475d0
Soumil Paranjpay <soumil.paranjpay@gmail.com>
2019-04-17 11:35:34 +0530

    feat: breadtags v2.0.0

--- src/events/commandUnknown.js
+++ src/events/commandUnknown.js
@@ -1,6 +1,5 @@
 const { Event } = require("klasa");
-const { Parser } = require("breadtags");
-const fs = require("fs-nextra");
+const Parser = require("breadtags");
 
 const timeout = new Set();
 
@@ -21,13 +20,7 @@ module.exports = class extends Event {
             member: msg.member,
             channel: msg.channel,
             args
-        }).catch(e => e);
-
-        if (typeof parsed === Error) {
-            throw `Hey, there was an Error with this Custom Command's Content. Please Screenshot the following error and Share in <https://pengubot.com/support>\n\n**Error Stack:**\`\`\`${parsed}\`\`\``;
-        }
-
-        if (!parsed || !parsed.length) return;
+        });
         await msg.channel.send(parsed);
         timeout.add(`${msg.author.id}-${msg.guild.id}`);
         setTimeout(() => timeout.delete(`${msg.author.id}-${msg.guild.id}`), 3500);
@@ -35,12 +28,8 @@ module.exports = class extends Event {
     }
 
     async init() {
-        this.parser = new Parser({ throwErrors: true });
-        const files = await fs.readdir("./lib/tags");
-        for (const file of files) {
-            if (file.includes("index")) continue;
-            Parser.loadTag(require(`../lib/tags/${file}`));
-        }
+        this.parser = new Parser();
+        this.parser.loadAll("../lib/tags");
     }
 
 };
