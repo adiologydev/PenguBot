64e34c921b24dab62b61e6be6005c9f71fe48239
Kashall <jordpjones@gmail.com>
2019-04-22 20:30:42 -0700

    fix: member roles not being added on level up

--- src/monitors/memberProfiles.js
+++ src/monitors/memberProfiles.js
@@ -20,15 +20,13 @@ module.exports = class extends Monitor {
         if (timeout.has(`${msg.author.id}-${msg.guild.id}`)) return;
 
         if (this.client.user.id !== "303181184718995457") {
-            const mainBot = msg.guild.members.get("303181184718995457");
-            if (mainBot) return;
+            if (msg.guild.members.get("303181184718995457")) return;
         }
 
         await msg.member.settings.sync(true);
         if (!msg.member.settings) return;
 
         const randomXP = this.client.funcs.randomNumber(1, 5);
-        const oldLvl = msg.member.settings.level;
         const newXP = msg.member.settings.xp + randomXP;
         const newLvl = Math.floor(0.2 * Math.sqrt(newXP));
         await msg.member.settings.update([["xp", newXP], ["level", newLvl]]);
@@ -37,12 +35,11 @@ module.exports = class extends Monitor {
         setTimeout(() => timeout.delete(`${msg.author.id}-${msg.guild.id}`), 45000);
 
         // Generate Level Up Images on Level Up
-        if (oldLvl !== newLvl) {
+        if (msg.member.settings.level !== newLvl) {
             if (msg.guild.settings.levelup) {
                 if (msg.guild.settings.leveltype !== "guild") return;
                 if (!msg.channel.postable) return;
-                const bgName = msg.author.settings.profilebg;
-                const bgImg = await fs.readFile(`../assets/profiles/bg/${bgName}.png`);
+                const bgImg = await fs.readFile(`../assets/profiles/bg/${msg.author.settings.profilebg}.png`);
                 const avatar = await get(msg.author.displayAvatarURL({ format: "png", size: 128 })).then(res => res.body).catch(e => {
                     Error.captureStackTrace(e);
                     return e;
@@ -51,12 +48,12 @@ module.exports = class extends Monitor {
                     .addImage(bgImg, 0, 0, 530, 530)
                     .addImage(avatar, 22, 22, 57, 57)
                     .toBufferAsync();
-                msg.sendMessage(`🆙 | **${msg.author.tag}** has leveled up to **Level ${newLvl}** in **${msg.guild.name}**`, { files: [{ attachment: img, name: `${msg.author.id}.png` }] });
+                return msg.sendMessage(`🆙 | **${msg.author.tag}** has leveled up to **Level ${newLvl}** in **${msg.guild.name}**`, { files: [{ attachment: img, name: `${msg.author.id}.png` }] });
             }
             if (msg.guild.settings.levelroles.enabled) {
                 const { roles } = msg.guild.settings.levelroles;
                 if (roles.length === 0) return;
-                const levelRole = roles.find(r => r.lvl === newLvl);
+                const levelRole = roles.find(r => r.lvl == newLvl);
                 if (!levelRole) return;
                 const role = msg.guild.roles.find(r => r.id === levelRole);
                 if (!role) return;
