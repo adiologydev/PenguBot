a381293e13896622dd1ce0c627fe4c67ea8532ba
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2019-04-24 03:25:16 +0530

    wip: new guild schema

--- src/lib/structures/schemas/defaultGuildSchema.js
+++ src/lib/structures/schemas/defaultGuildSchema.js
@@ -1,22 +1,17 @@
+/* eslint-disable no-inline-comments */
 const { KlasaClient } = require("klasa");
 
 module.exports = KlasaClient.defaultGuildSchema
 
-    // Custom Commands
-    .add("customcmds", folder => folder
-        .add("cmds", "any", { array: true, configurable: false })
-        .add("enabled", "boolean", { default: true }))
-
-    // Inhibitors
-    .add("disabledCommandsGroup", "string", { array: true, configurable: false })
+// DEPRECATED START -------------------
 
     // Auto Roles
-    .add("autoroles", folder => folder
+    .add("autoroles", folder => folder // delete after transfer
         .add("roles", "role", { array: true })
         .add("enabled", "boolean", { default: true }))
 
     // Logging
-    .add("loggingChannel", "textchannel")
+    .add("loggingChannel", "textchannel") // delete after transfer
     .add("logs", folder => folder
         .add("join", "boolean", { default: false })
         .add("leave", "boolean", { default: false })
@@ -24,24 +19,7 @@ module.exports = KlasaClient.defaultGuildSchema
         .add("messages", "boolean", { default: false })
         .add("roles", "boolean", { default: false }))
 
-    // Starboard
-    .add("starboard", folder => folder
-        .add("enabled", "boolean", { default: true })
-        .add("channel", "textchannel")
-        .add("required", "integer", { default: 3 }))
-
-    // Welcome & Leave
-    .add("messages", folder => folder
-        .add("leave", leave => leave
-            .add("enabled", "boolean", { default: false })
-            .add("channel", "textchannel")
-            .add("message", "string", { default: "It's sad to see you leave {USERNAME}, hope to see you again." }))
-        .add("welcome", welcome => welcome
-            .add("enabled", "boolean", { default: false })
-            .add("channel", "textchannel")
-            .add("message", "string", { default: "Welcome {MENTION} to {GUILD_NAME}, we hope you enjoy your stay!" })))
-
-    // Permissions
+    // Permissions - delete after transfer
     .add("permissions", folder => folder
         .add("dj", "user", { array: true })
         .add("djRole", "role", { array: true })
@@ -51,9 +29,49 @@ module.exports = KlasaClient.defaultGuildSchema
         .add("modsRole", "role", { array: true })
         .add("mutedRole", "role", { configurable: false }))
 
+    // Self Roles - NEW FOLDER
+    .add("selfroles", folder => folder
+        .add("roles", "role", { array: true })
+        .add("enabled", "boolean", { default: true }))
+
+    // Level Roles
+    .add("levelroles", folder => folder // delete after transfer
+        .add("roles", "any", { array: true, configurable: false })
+        .add("enabled", "boolean", { default: true }))
+
+    .add("djOnly", "boolean", { default: false }) // delete after transfer
+    .add("levelup", "boolean", { default: false }) // delete after transfer
+
+// DEPRECATED END ---------------------
+
+    // Custom Commands
+    .add("customcmds", folder => folder
+        .add("cmds", "any", { array: true, configurable: false })
+        .add("enabled", "boolean", { default: true })) // delete after transfer
+
+    // Starboard
+    .add("starboard", folder => folder
+        .add("enabled", "boolean", { default: true }) // delete after transfer
+        .add("channel", "textchannel")
+        .add("required", "integer", { default: 3 }))
+
+    // Inhibitors
+    .add("disabledCommandsGroup", "string", { array: true, configurable: false })
+
+    // Welcome & Leave
+    .add("messages", folder => folder
+        .add("leave", leave => leave
+            .add("enabled", "boolean", { default: false }) // delete after transfer
+            .add("channel", "textchannel") // delete after transfer
+            .add("message", "string", { default: "It's sad to see you leave {USERNAME}, hope to see you again." }))
+        .add("welcome", welcome => welcome
+            .add("enabled", "boolean", { default: false }) // delete after transfer
+            .add("channel", "textchannel") // delete after transfer
+            .add("message", "string", { default: "Welcome {MENTION} to {GUILD_NAME}, we hope you enjoy your stay!" })))
+
     // Automod
     .add("automod", folder => folder
-        .add("enabled", "boolean", { default: false })
+        .add("enabled", "boolean", { default: false }) // delete after transfer
         .add("filters", filter => filter
             .add("TOXICITY", "any", { default: { enabled: false, threshold: 0.99 } })
             .add("SEVERE_TOXICITY", "any", { default: { enabled: false, threshold: 0.99 } })
@@ -64,16 +82,6 @@ module.exports = KlasaClient.defaultGuildSchema
             .add("PROFANITY", "any", { default: { enabled: false, threshold: 0.99 } }))
         .add("invites", "boolean", { default: false }))
 
-    // Self Roles
-    .add("selfroles", folder => folder
-        .add("roles", "role", { array: true })
-        .add("enabled", "boolean", { default: true }))
-
-    // Level Roles
-    .add("levelroles", folder => folder
-        .add("roles", "any", { array: true, configurable: false })
-        .add("enabled", "boolean", { default: true }))
-
     // Patreon
     .add("patreon", folder => folder
         .add("enabled", "boolean", { default: false, configurable: false })
@@ -81,9 +89,9 @@ module.exports = KlasaClient.defaultGuildSchema
 
     // Misc
     .add("musicVolume", "integer", { default: 100, max: 100, min: 0 })
-    .add("djOnly", "boolean", { default: false })
-    .add("levelup", "boolean", { default: false })
     .add("leveltype", "string", { default: "guild", configurable: false })
+
+// New Schema Start -------
 
     // Mod Logs
     .add("modlogs", folder => folder
@@ -101,7 +109,29 @@ module.exports = KlasaClient.defaultGuildSchema
     // All Channels
     .add("channels", folder => folder
         .add("modlogs", "textchannel")
-        .add("starboard", "textchannel")
         .add("join", "textchannel")
         .add("leave", "textchannel")
-        .add("logs", "textchannel"));
+        .add("logs", "textchannel"))
+
+    // Toggles
+    .add("toggles", folder => folder
+        .add("autorole", "boolean", { default: true })
+        .add("perspective", "boolean", { default: true })
+        .add("levelroles", "boolean", { default: true })
+        .add("djmode", "boolean", { default: false })
+        .add("levelup", "boolean", { default: false })
+        .add("selfrole", "boolean", { default: true }))
+
+    // Permissions
+    .add("users", users => users
+        .add("admin", "user", { array: true })
+        .add("mod", "user", { array: true })
+        .add("dj", "user", { array: true }))
+    .add("roles", roles => roles
+        .add("autorole", "role", { array: true })
+        .add("selfrole", "role", { array: true })
+        .add("leveledrole", "any", { array: true, configurable: false })
+        .add("admin", "role")
+        .add("mod", "role")
+        .add("dj", "role")
+        .add("muted", "role", { configurable: false }));
