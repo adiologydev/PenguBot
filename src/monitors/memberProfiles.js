8da257078abce49fd1eb4d8c1f659824d52a2bd6
Jack <jacknagle22@gmail.com>
2019-04-24 04:47:53 +1000

    Finished memberProfiles

--- src/monitors/memberProfiles.js
+++ src/monitors/memberProfiles.js
@@ -16,57 +16,61 @@ module.exports = class extends Monitor {
     }
 
     async run(msg) {
-        if (!msg.guild || !msg.member) return;
+        if (!msg.guild) return;
         if (timeout.has(`${msg.author.id}-${msg.guild.id}`)) return;
 
-        if (this.client.user.id !== "303181184718995457") {
-            const mainBot = msg.guild.members.get("303181184718995457");
-            if (mainBot) return;
-        }
+        if (this.client.user.id !== "303181184718995457" && msg.guild.members.has("303181184718995457")) return;
 
         await msg.member.settings.sync(true);
-        if (!msg.member.settings) return;
 
         const randomXP = this.client.funcs.randomNumber(1, 5);
-        const oldLvl = msg.member.settings.level;
+        const oldLevel = msg.member.settings.level;
         const newXP = msg.member.settings.xp + randomXP;
-        const newLvl = Math.floor(0.2 * Math.sqrt(newXP));
-        await msg.member.settings.update([["xp", newXP], ["level", newLvl]]);
+        const newLevel = Math.floor(0.2 * Math.sqrt(newXP));
+        await msg.member.settings.update([["xp", newXP], ["level", newLevel]]);
 
         timeout.add(`${msg.author.id}-${msg.guild.id}`);
         setTimeout(() => timeout.delete(`${msg.author.id}-${msg.guild.id}`), 45000);
 
-        // Generate Level Up Images on Level Up
-        if (oldLvl !== newLvl) {
-            if (msg.guild.settings.levelup) {
-                if (msg.guild.settings.leveltype !== "guild") return;
-                if (!msg.channel.postable) return;
-                const bgName = msg.author.settings.profilebg;
-                const bgImg = await fs.readFile(`../assets/profiles/bg/${bgName}.png`);
-                const avatar = await get(msg.author.displayAvatarURL({ format: "png", size: 128 })).then(res => res.body).catch(e => {
-                    Error.captureStackTrace(e);
-                    return e;
-                });
-                const img = await new Canvas(100, 100)
-                    .addImage(bgImg, 0, 0, 530, 530)
-                    .addImage(avatar, 22, 22, 57, 57)
-                    .toBufferAsync();
-                msg.sendMessage(`🆙 | **${msg.author.tag}** has leveled up to **Level ${newLvl}** in **${msg.guild.name}**`, { files: [{ attachment: img, name: `${msg.author.id}.png` }] });
-            }
+        if (oldLevel === newLevel) return;
 
-            if (msg.guild.settings.levelroles.enabled) {
-                const { roles } = msg.guild.settings.levelroles;
-                if (!roles.length) return;
-                const levelRole = roles.find(r => r.lvl === newLvl);
-                if (!levelRole) return;
-                const role = msg.guild.roles.find(r => r.id === levelRole);
-                if (!role) return;
-                const myRole = msg.guild.me.roles.find(r => r.managed);
-                if (role.position > myRole.positon) return;
-                if (msg.member.roles.has(role)) return;
-                await msg.member.roles.add(role, "Level Based Role").catch(() => null);
-            }
-        }
+        const { settings } = msg.guild;
+
+        if (settings.levelup && settings.leveltype === "guild") await this.handleLevelup(msg);
+        if (settings.levelroles.enabled && settings.levelroles.roles.length) await this.leveledroles(msg);
+    }
+
+    async handleLevelup(msg) {
+        if (!msg.channel.postable) return;
+
+        const image = await this.generateLevelUpImage(msg.author.settings.profilebg, msg.author.displayAvatarURL({ format: "png", size: 128 }));
+        return msg.sendMessage(`🆙 | **${msg.author.tag}** has leveled up to **Level ${msg.member.settings.level}** in **${msg.guild.name}**`, { files: [{ attachment: image, name: `${msg.author.id}.png` }] });
+    }
+
+    async leveledroles(msg) {
+        const levelRole = msg.guild.settings.levelroles.roles.find(r => r.lvl === msg.member.settings.level);
+        if (!levelRole) return;
+
+        if (msg.member.roles.has(levelRole.id)) return;
+
+        const role = msg.guild.roles.get(levelRole.id);
+        if (!role) return;
+
+        if (role.position >= msg.guild.me.roles.highest.position) return;
+
+        await msg.member.roles.add(role, "Level Based Role").catch(() => null);
+    }
+
+    async generateLevelUpImage(background, userAvatar) {
+        const [backgroundImage, avatar] = await Promise.all([
+            fs.readFile(`../assets/profiles/bg/${background}.png`),
+            get(userAvatar).then(res => res.body).catch(() => null)
+        ]);
+
+        return new Canvas(100, 100)
+            .addImage(backgroundImage, 0, 0, 530, 530)
+            .addImage(avatar, 22, 22, 57, 57)
+            .toBufferAsync();
     }
 
 };
