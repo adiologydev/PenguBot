5b02090f34f178c9674e3176178d2fa41fa84583
Jack <jacknagle22@gmail.com>
2019-04-25 08:46:05 +1000

    Done profiles

--- src/monitors/profiles.js
+++ src/monitors/profiles.js
@@ -17,41 +17,39 @@ module.exports = class extends Monitor {
 
     async run(msg) {
         if (!msg.guild) return;
-        if (timeout.has(msg.author.id)) return;
+        if (timeout.has(`${msg.guild.id}-${msg.author.id}`)) return;
 
         if (this.client.user.id !== "303181184718995457" && await msg.guild.members.fetch("303181184718995457").catch(() => null)) return;
 
         await msg.author.settings.sync(true);
-        if (!msg.author.settings) return;
 
         const randomXP = this.client.funcs.randomNumber(1, 5);
         const randomSnowflakes = this.client.funcs.randomNumber(1, 2);
         const newSnowflakes = msg.author.settings.snowflakes + randomSnowflakes;
         const newXP = msg.author.settings.xp + randomXP;
-        const oldLvl = msg.author.settings.level;
-        const newLvl = Math.floor(0.2 * Math.sqrt(newXP));
-        await msg.author.settings.update([["xp", newXP], ["level", newLvl], ["snowflakes", newSnowflakes]]);
+        const oldLevel = msg.author.settings.level;
+        const newLevel = Math.floor(0.2 * Math.sqrt(newXP));
+        await msg.author.settings.update([["xp", newXP], ["level", newLevel], ["snowflakes", newSnowflakes]]);
 
-        timeout.add(msg.author.id);
-        setTimeout(() => timeout.delete(msg.author.id), 45000);
+        timeout.add(`${msg.guild.id}-${msg.author.id}`);
+        setTimeout(() => timeout.delete(`${msg.guild.id}-${msg.author.id}`), 45000);
 
-        // Generate Level Up Images on Level Up
-        if (oldLvl !== newLvl) {
-            if (!msg.guild.settings.levelup) return;
-            if (msg.guild.settings.leveltype !== "user") return;
-            if (!msg.channel.postable) return;
-            const bgName = msg.author.settings.profilebg;
-            const bgImg = await fs.readFile(`../assets/profiles/bg/${bgName}.png`);
-            const avatar = await get(msg.author.displayAvatarURL({ format: "png", size: 128 })).then(res => res.body).catch(e => {
-                Error.captureStackTrace(e);
-                return e;
-            });
-            const img = await new Canvas(100, 100)
-                .addImage(bgImg, 0, 0, 530, 530)
-                .addImage(avatar, 22, 22, 57, 57)
-                .toBufferAsync();
-            msg.sendMessage(`🆙 | **${msg.author.tag} has leveled up to Level ${newLvl}!**`, { files: [{ attachment: img, name: `${msg.author.id}.png` }] });
-        }
+        if (oldLevel === newLevel && !msg.guild.settings.levelup && msg.guild.settings.leveltype !== "user" && !msg.channel.postable) return;
+
+        const image = await this.generateLevelUpImage(msg.author.settings.profilebg, msg.author.displayAvatarURL({ format: "png", size: 128 }));
+        return msg.sendMessage(`🆙 | **${msg.author.tag} has leveled up to Level ${newLevel}!**`, { files: [{ attachment: image, name: `${msg.author.id}.png` }] });
+    }
+
+    async generateLevelUpImage(background, userAvatar) {
+        const [backgroundImage, avatar] = await Promise.all([
+            fs.readFile(`../assets/profiles/bg/${background}.png`),
+            get(userAvatar).then(res => res.body).catch(() => null)
+        ]);
+
+        return new Canvas(100, 100)
+            .addImage(backgroundImage, 0, 0, 530, 530)
+            .addImage(avatar, 22, 22, 57, 57)
+            .toBufferAsync();
     }
 
 };
