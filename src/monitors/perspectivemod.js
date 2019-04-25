da3955e6693a1506e1d8a587f93c50d24876c3b1
Jack <jacknagle22@gmail.com>
2019-04-25 14:05:36 +1000

    Cleanup(perspectivemod): Cleanup and bit of improvment

--- src/monitors/perspectivemod.js
+++ src/monitors/perspectivemod.js
@@ -8,41 +8,29 @@ module.exports = class extends Monitor {
     }
 
     async run(msg) {
-        if (!msg.guild || !msg.guild.settings.automod.enabled || !msg.content) return;
-        if (msg.content.startsWith(msg.guild.settings.prefix) || this.mentionPrefix(msg)) return;
+        if (!msg.guild || !msg.content || msg.command || !msg.guild.settings.automod.enabled) return;
         if (await msg.hasAtLeastPermissionLevel(4)) return;
 
         if (this.client.user.id !== "303181184718995457" && await msg.guild.members.fetch("303181184718995457").catch(() => null)) return;
 
-        const req = await post(`https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${this.client.config.keys.perspective}`)
+        const { body } = await post(`https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${this.client.config.keys.perspective}`)
             .send({ comment: { text: msg.content }, requestedAttributes: { SEVERE_TOXICITY: {}, TOXICITY: {}, OBSCENE: {}, THREAT: {}, SEXUALLY_EXPLICIT: {}, SPAM: {}, PROFANITY: {} } })
-            .catch(() => null);
+            .catch(() => ({ body: null }));
 
-        if (!req) return;
+        if (!body) return;
 
         const { filters } = msg.guild.settings.automod;
 
-        for (const key of Object.keys(req.body.attributeScores)) {
-            if (filters[key].enabled) {
-                if (req.body.attributeScores[key].summaryScore.value >= filters[key].threshold) {
-                    try {
-                        msg.delete();
-                        this.client.emit("customLogs", msg.guild, "automod", { filter: key, channel: msg.channel, name: "automod", content: msg.content, image: msg.attachments.size > 0 ? await this.checkAttachments(msg.attachments.array()[0].url) : null }, msg.author);
-                    } catch (e) {
-                        return;
-                    }
-                }
-            }
+        for (const key of Object.keys(body.attributeScores)) {
+            if (!filters[key].enabled) continue;
+            if (!body.attributeScores[key].summaryScore.value >= filters[key].threshold) return;
+            await msg.delete().catch(() => null);
+            this.client.emit("customLogs", msg.guild, "automod", { filter: key, channel: msg.channel, name: "automod", content: msg.content, image: msg.attachments.size > 0 ? await this.checkAttachments(msg.attachments.first()) : null }, msg.author);
         }
     }
 
-    mentionPrefix({ content }) {
-        const prefixMention = this.prefixMention.exec(content);
-        return prefixMention ? { length: prefixMention[0].length, regex: this.prefixMention } : null;
-    }
-
-    init() {
-        this.prefixMention = new RegExp(`^<@!?${this.client.user.id}>`);
+    checkAttachments(attachment) {
+        return attachment && attachment.height ? attachment : null;
     }
 
 };
