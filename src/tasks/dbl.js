74b66dbade0cecea9a4d989834f7ba5c2975de1a
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2019-04-25 17:02:45 +0530

    update: stats posting

--- src/tasks/dbl.js
+++ src/tasks/dbl.js
@@ -5,15 +5,15 @@ module.exports = class extends Task {
 
     async run() {
         if (this.client.user.id !== "303181184718995457") return;
-        const stats = { server_count: this.client.guilds.size, shard_id: this.client.shard.id, shard_count: this.client.shard.count };
+        const stats = { server_count: this.client.guilds.size, shard_count: this.client.shard.shardCount };
 
         let [guilds, vc, users] = [0, 0, 0];
-        const results = await this.client.shard.broadcastEval(`[this.guilds.reduce((prev, val) => val.memberCount + prev, 0), this.guilds.size, this.lavalink.size]`);
 
+        const results = await this.client.shard.broadcastEval(`[this.guilds.reduce((prev, val) => val.memberCount + prev, 0), this.guilds.size, this.lavalink.map(u => u).filter(p => p.playing).length]`);
         for (const result of results) {
             users += result[0];
-            vc = +result[3];
             guilds += result[1];
+            vc += result[2];
         }
 
         return Promise.all([
@@ -25,8 +25,8 @@ module.exports = class extends Task {
                 .set("Authorization", `Bot ${this.client.config.keys.ogdbl}`).send({ guilds: guilds, users: users, shard_id: 0, voice_connections: vc }),
             snekfetch.post(`https://botsfordiscord.com/api/bot/${this.client.user.id}`)
                 .set("Authorization", this.client.config.keys.b4d).send({ server_count: guilds }),
-            snekfetch.post(`https://discordbots.group/api/bot/${this.client.user.id}`)
-                .set("Authorization", this.client.config.keys.dbg).send({ count: guilds })
+            snekfetch.post(`https://api.discordbots.group/v1/bot/${this.client.user.id}`)
+                .set("Authorization", this.client.config.keys.dbg).send({ server_count: guilds })
         ]);
     }
 
