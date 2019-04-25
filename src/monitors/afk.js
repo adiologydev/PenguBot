0a5288ec81329e58bee9ff48ebdbf9df6a6233f1
Jack <jacknagle22@gmail.com>
2019-04-25 14:50:53 +1000

    Monitor(afk): refactor afk monitor. also updated EN-US language for new afk strings.

--- src/monitors/afk.js
+++ src/monitors/afk.js
@@ -7,22 +7,26 @@ module.exports = class extends Monitor {
     }
 
     async run(msg) {
-        if (!msg.guild || !msg.channel.postable || msg.author.id === this.client.user.id) return;
+        if (!msg.guild || !msg.channel.postable) return;
 
         if (this.client.user.id !== "303181184718995457" && await msg.guild.members.fetch("303181184718995457").catch(() => null)) return;
 
-        if (msg.mentions.users.size) {
-            const mentioned = msg.mentions.users.first();
-            if (mentioned.settings.afk.afk) {
-                msg.sendMessage(`⏰ | ***${mentioned.tag} ${msg.language.get("MESSAGE_IS_AFK")}*** ${mentioned.settings.afk.reason}`);
-            }
-        }
+        if (msg.mentions.users.size) await this.afkMentioned(msg);
+        if (msg.author.settings.afk.time) await this.checkAfk(msg);
+    }
 
-        if (msg.author.settings.afk.afk) {
-            await msg.author.settings.update([["afk.afk", false], ["afk.reason", null]]);
-            const m = await msg.sendMessage(`<:penguError:435712890884849664> ***${msg.author.tag} ${msg.language.get("MESSAGE_AFK_REMOVED")}***`);
-            await m.delete({ timeout: 10000 });
-        }
+    async checkAfk(msg) {
+        await msg.author.settings.reset("afk");
+
+        const message = await msg.sendLocale("MONITOR_AFK_REMOVED", [msg.author.username]);
+        return message.delete({ timeout: 10000, reason: "Pengubot AFK Feature" }).catch(() => null);
+    }
+
+    async afkMentioned(msg) {
+        const mentioned = msg.mentions.users.first();
+        const { afk } = mentioned.settings;
+
+        if (afk.time) return msg.sendLocale("MONITOR_AFK_ISAFK", [mentioned.username, afk.reason, afk.time]);
     }
 
 };
