63f96ca01a851d0df15d65a97f6ba117a988195d
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2019-04-29 01:56:00 +0530

    fix: transferSetting to not skip guilds

--- src/tasks/transferSettings.js
+++ src/tasks/transferSettings.js
@@ -16,58 +16,66 @@ module.exports = class extends Task {
             if (language) settings.language = language;
             if (disabledCommands) settings.disabledCommands = disabledCommands;
 
-            if (!permissions) continue;
-            if (permissions.mods) settings.users.mod = permissions.mods;
-            if (permissions.admins) settings.users.admin = permissions.admins;
-            if (permissions.dj) settings.users.dj = permissions.dj;
+            if (permissions) {
+                if (permissions.mods) settings.users.mod = permissions.mods;
+                if (permissions.admins) settings.users.admin = permissions.admins;
+                if (permissions.dj) settings.users.dj = permissions.dj;
+            }
 
-            if (!autoroles) continue;
-            if (autoroles.roles) settings.roles.autorole = autoroles.roles;
-            settings.toggles.autorole = autoroles.enabled;
+            if (autoroles) {
+                if (autoroles.roles) settings.roles.autorole = autoroles.roles;
+                settings.toggles.autorole = autoroles.enabled;
+            }
 
-            if (!selfroles) continue;
-            if (selfroles.roles) settings.roles.selfrole = selfroles.roles;
-            settings.toggles.selfrole = selfroles.enabled;
+            if (selfroles) {
+                if (selfroles.roles) settings.roles.selfrole = selfroles.roles;
+                settings.toggles.selfrole = selfroles.enabled;
+            }
 
-            if (!levelroles) continue;
-            if (levelroles.roles) settings.roles.levelrole = levelroles.roles;
-            settings.toggles.levelroles = levelroles.enabled;
+            if (levelroles) {
+                if (levelroles.roles) settings.roles.levelrole = levelroles.roles;
+                settings.toggles.levelroles = levelroles.enabled;
+            }
 
-            if (!join) continue;
-            if (join.channel) settings.channels.join = join.channel;
-            if (join.message) settings.messages.join = join.message;
-            settings.toggles.joinmsg = join.enabled;
+            if (join) {
+                if (join.channel) settings.channels.join = join.channel;
+                if (join.message) settings.messages.join = join.message;
+                settings.toggles.joinmsg = join.enabled;
+            }
 
-            if (!leave) continue;
-            if (leave.channel) settings.channels.leave = leave.channel;
-            if (leave.message) settings.messages.leave = leave.message;
-            settings.toggles.leavemsg = leave.enabled;
+            if (leave) {
+                if (leave.channel) settings.channels.leave = leave.channel;
+                if (leave.message) settings.messages.leave = leave.message;
+                settings.toggles.leavemsg = leave.enabled;
+            }
 
             if (loggingChannel) settings.channels.logs = loggingChannel;
             if (musicVolume) settings.misc.volume = musicVolume;
             if (leveltype) settings.misc.leveluptype = leveltype;
 
-            if (!automod) continue;
-            if (automod.filters) settings.automod.perspective = automod.filters;
-            settings.toggles.perspective = automod.enabled;
-            settings.automod.invites = automod.invites;
+            if (automod) {
+                if (automod.filters) settings.automod.perspective = automod.filters;
+                settings.toggles.perspective = automod.enabled;
+                settings.automod.invites = automod.invites;
+            }
 
             if (disabledCommandsGroup) settings.disabledCommandsGroup = disabledCommandsGroup;
 
             settings.toggles.djmode = djOnly;
             settings.toggles.levelup = levelup;
 
-            if (!customcmds) continue;
-            if (customcmds.cmds) settings.customcmds = customcmds.cmds;
-            settings.toggles.customcmds = customcmds.enabled;
+            if (customcmds) {
+                if (customcmds.cmds) settings.customcmds = customcmds.cmds;
+                settings.toggles.customcmds = customcmds.enabled;
+            }
 
-            if (!starboard) continue;
-            if (starboard.channel) settings.starboard.channel = starboard.channel;
-            if (starboard.required) settings.starboard.required = starboard.required;
-            settings.toggles.starboard = starboard.enabled;
+            if (starboard) {
+                if (starboard.channel) settings.starboard.channel = starboard.channel;
+                if (starboard.required) settings.starboard.required = starboard.required;
+                settings.toggles.starboard = starboard.enabled;
+            }
 
-            if (!logs) continue;
-            settings.serverlogs = logs;
+            if (logs) settings.serverlogs = logs;
 
             promises.push(this.provider.replace("guild", guild.id, settings));
         }
