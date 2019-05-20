972725b4d0d1c4cde8168fea0540ad3844b9aa34
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2019-05-21 03:46:00 +0530

    update: better errors in routes

--- src/routes/kdhGuild.js
+++ src/routes/kdhGuild.js
@@ -12,7 +12,7 @@ module.exports = class extends Route {
 
         const guild = await this.client.shard.fetchGuild(id).catch(() => null);
 
-        if (!guild) return response.end("Guild not found");
+        if (!guild) return response.status(200).json({ error: "Guild not found" });
         return response.end(JSON.stringify(guild));
     }
 
--- src/routes/oauthGuilds.js
+++ src/routes/oauthGuilds.js
@@ -11,15 +11,14 @@ module.exports = class extends Route {
     }
 
     async post(request, response) {
-        const findGuild = await this.client.shard.broadcastEval(`this.guilds.get("${request.body.id}")`);
-        const guild = findGuild.find(g => g);
-        if (!guild) return response.end("Guild not found");
+        const guildExists = await this.client.shard.fetchGuild(request.body.id).catch(() => null);
+        if (!guildExists) return response.status(200).json({ error: "Guild not found" });
 
         const updateGuild = await this.client.shard.broadcastEval(`if(this.guilds.get("${request.body.id}")) this.guilds.get("${request.body.id}").settings.update(${request.body.data}, { action: "overwrite" });`);
         const updated = updateGuild.find(g => g);
-        if (!updated) return response.end("Guild not found");
+        if (!updated) return response.status(200).json({ error: "Guild not found" });
 
-        if (updated.errors.length) this.client.emit("error", `${guild.name}[${guild.id}] failed updating guild configs via dashboard with error:\n${inspect(updated.errors)}`);
+        if (updated.errors.length) this.client.emit("error", `${guildExists.name}[${guildExists.id}] failed updating guild configs via dashboard with error:\n${inspect(updated.errors)}`);
         return response.end(RESPONSES.UPDATED[Number(!updated.errors.length)]);
     }
 
