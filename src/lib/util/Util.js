2ff97ef77684a9954c7538e631a752ff62801315
Jack <jacz@jaczaus.me>
2019-06-02 20:08:35 +1000

    Command(emote): Rewrite how we parse the given argument and also check if the emoji is valid

--- src/lib/util/Util.js
+++ src/lib/util/Util.js
@@ -2,10 +2,26 @@ const fetch = require("node-fetch");
 
 class Util {
 
+    static toCodePoint(unicode) {
+        const parts = [];
+        let c = 0, p = 0, i = 0;
+        while (i < unicode.length) {
+            c = unicode.charCodeAt(i++);
+            if (p) {
+                parts.push((0x10000 + ((p - 0xD800) << 10) + (c - 0xDC00)).toString(16)); // eslint-disable-line no-bitwise
+                p = 0;
+            } else if (c >= 0xD800 && c <= 0xDBFF) {
+                p = c;
+            } else {
+                parts.push(c.toString(16));
+            }
+        }
+        return parts.join("-");
+    }
+
     static haste(data, extension = "js") {
         return Util.fetch("https://paste.pengubot.com/documents", { method: "post", body: data })
-            .then(body => `https://paste.pengubot.com/${body.key}.${extension}`)
-            .catch(error => error.statusText ? error.statusText : error.message);
+            .then(body => `https://paste.pengubot.com/${body.key}.${extension}`);
     }
 
     static showSeconds(ms) {
