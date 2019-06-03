5a0c8650b26542d02a6404ed1eb82f42d87b6599
Jack <jacz@jaczaus.me>
2019-06-03 20:07:53 +1000

    Command(translate): Fix

--- src/commands/Utilities/translate.js
+++ src/commands/Utilities/translate.js
@@ -1,11 +1,10 @@
-const Command = require("../../lib/structures/KlasaCommand");
+const { Command } = require("../../index");
 const translate = require("@k3rn31p4nic/google-translate-api");
 
 module.exports = class extends Command {
 
     constructor(...args) {
         super(...args, {
-            runIn: ["text", "dm"],
             description: language => language.get("COMMAND_TRANSLATE_DESCRIPTION"),
             usageDelim: "|",
             usage: "<content:string> <language:str>"
@@ -13,8 +12,10 @@ module.exports = class extends Command {
     }
 
     async run(msg, [content, language]) {
-        return translate(content, { to: `${language}` }).then(res => msg.sendMessage(`📗 **Translated Message:** ${res.text}`))
-            .catch(() => msg.sendMessage(`${this.client.emotes.cross} ***That language is not supported, please try again.***`));
+        const { text } = await translate(content, { to: language })
+            .catch(() => { throw "That language is not supported, please try again." });
+        
+        return msg.sendMessage(`**Translated Message:** ${text}`);
     }
 
 };
