6870d9d0f0756edec031c76cd6a38592a81413e7
Jack <jacz@jaczaus.me>
2019-06-03 23:06:23 +1000

    Lint

--- src/commands/Utilities/translate.js
+++ src/commands/Utilities/translate.js
@@ -13,8 +13,8 @@ module.exports = class extends Command {
 
     async run(msg, [content, language]) {
         const { text } = await translate(content, { to: language })
-            .catch(() => { throw "That language is not supported, please try again." });
-        
+            .catch(() => { throw "That language is not supported, please try again."; });
+
         return msg.sendMessage(`**Translated Message:** ${text}`);
     }
 
