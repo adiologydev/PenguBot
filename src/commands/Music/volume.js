d8f1cb9e31fba3dd0c6a1f5beae31b723303bcab
Jack <jacz@jaczaus.me>
2019-06-15 15:13:50 +1000

    Update(MusicInterface): Await all the things and remove all the things

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -1,9 +1,10 @@
-const { MusicCommand, config } = require("../../index");
+const { MusicCommand } = require("../../index");
 
 module.exports = class extends MusicCommand {
 
     constructor(...args) {
         super(...args, {
+            patronOnly: true,
             requireDJ: true,
             requireMusic: true,
             cooldown: 8,
@@ -16,10 +17,8 @@ module.exports = class extends MusicCommand {
     }
 
     async run(msg, [volume]) {
-        if (!config.patreon) throw msg.sendMessage(`${this.client.emotes.cross} ***${msg.language.get("CMD_PATRON_ONLY")}***`);
         if (!volume) return msg.sendMessage(`🔈 | ***Guild's Current Music Volume is:*** ${msg.guild.settings.misc.volume}`);
-        if (!await msg.hasAtLeastPermissionLevel(3)) return msg.reply(`${this.client.emotes.cross} You are not a **Pengu DJ** to change the volume.`);
-        if (volume < 0 || volume > 100) return msg.sendMessage(`${this.client.emotes.cross} ***Volume can not be lower than 0 or higher than 100.***`);
+        if (volume <= 0 || volume >= 100) return msg.sendMessage(`${this.client.emotes.cross} ***Volume can not be lower than 0 or higher than 100.***`);
         await msg.guild.settings.update("misc.volume", volume);
         if (msg.guild.music.playing) msg.guild.music.player.volume(volume);
         return msg.sendMessage(`${this.client.emotes.check} ***Volume has been set to:*** ${volume}`);
--- src/lib/structures/MusicInterface.js
+++ src/lib/structures/MusicInterface.js
@@ -13,11 +13,6 @@ class MusicInterface {
         this.looping = false;
     }
 
-    /**
-     * Joins the voice channel which the bot will play tracks in
-     * @param {VoiceChannel} voiceChannel The voice channel the bot is connected to
-     * @returns {string}
-     */
     join(voiceChannel) {
         return this.client.lavalink.join({
             guild: this.guild.id,
@@ -26,11 +21,6 @@ class MusicInterface {
         }, { selfdeaf: true });
     }
 
-    /**
-     * Leaves the voice channel the bot is connected to
-     * @param {boolean} force Force leave option
-     * @returns {MusicInterface}
-     */
     async leave(force = true) {
         if (this.player && (this.playing || force)) this.player.stop();
         await this.client.lavalink.leave(this.guild.id);
@@ -38,64 +28,49 @@ class MusicInterface {
         return this;
     }
 
-    /**
-     * Gets the most ideal region based on specified guild region.
-     * @returns {Player}
-     */
     async play() {
         if (!this.player) throw "No! Something went wrong, try again.";
         else if (!this.queue.length) throw "Can't play songs from an empty queue.";
 
         const [song] = this.queue;
-        this.player.play(song.track);
-        if (config.patreon) this.player.volume(this.volume);
+        const volume = config.patreon ? { volume: this.guild.settings.get("misc.volume") } : {};
+
+        await this.player.play(song.track, volume);
+
         this.playing = true;
         return this.player;
     }
 
-    /**
-     * Pause/Resume the player
-     * @returns {boolean}
-     */
-    pause() {
-        if (!this.player) return false;
-        this.player.pause(!this.paused);
-        this.paused = !this.paused;
-        return true;
+    async pause(pause = true) {
+        if (!this.player) return null;
+        await this.player.pause(pause);
+        this.paused = pause;
+        return this.paused;
     }
 
-    /**
-     * Skips the current song in the queue
-     * @param {boolean} force Force skip option
-     * @returns {MusicInterface}
-     */
-    skip(force = true) {
-        if (this.player && force) this.player.stop();
+    resume() {
+        return this.pause(false);
+    }
+
+    async skip(force = true) {
+        if (this.player && force) await this.player.stop();
         else this.queue.shift();
         return this;
     }
 
-    /**
-     * Clears the current queue
-     * @returns {MusicInterface}
-     */
     clearQueue() {
         this.queue = [];
         return this;
     }
 
-    /**
-     * Destroys the current interface
-     * @returns {void}
-     */
     async destroy() {
-        this.queue = [];
+        this.queue = null;
         this.playing = null;
         this.paused = null;
         this.textChannel = null;
         this.looping = null;
 
-        if (this.player) this.player.destroy();
+        if (this.player) await this.player.destroy();
         await this.leave();
         this.client.music.delete(this.guild.id);
     }
@@ -108,16 +83,8 @@ class MusicInterface {
         return this.client.lavalink.players.get(this.guild.id) || null;
     }
 
-    get volume() {
-        return this.guild.settings.get("misc.volume");
-    }
-
     get idealNode() {
         return this.client.lavalink.idealNodes.first();
-    }
-
-    isListening(member) {
-        return !member.voice.deaf && member.voice.channel.id === this.voiceChannel.id;
     }
 
 }
