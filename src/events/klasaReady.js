7a5979f3980cab80d45d858a4bd513911396a152
Jack <jacz@jaczaus.me>
2019-06-20 11:31:15 +1000

    Add shards to LavalinkClient

--- src/events/klasaReady.js
+++ src/events/klasaReady.js
@@ -5,7 +5,10 @@ module.exports = class extends Event {
 
     async run() {
         // Setup lavalink
-        this.client.lavalink = new LavalinkClient(this.client, config.nodes, { user: this.client.user });
+        this.client.lavalink = new LavalinkClient(this.client, config.nodes, {
+            user: this.client.user,
+            shards: this.client.shard.count
+        });
         this.client.console.log(`[${this.client.shard.id}]: Online`);
     }
 
