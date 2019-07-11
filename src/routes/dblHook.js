cf1b66292814a422d8438b91451a30b3da6c3ed0
Jack <jacz@jaczaus.me>
2019-07-11 17:10:48 +1000

    Fix this shitz, p.s using .catch(() => null) because fetchGuild returns its own error and we dont want that

--- src/routes/dblHook.js
+++ src/routes/dblHook.js
@@ -9,11 +9,18 @@ module.exports = class extends Route {
     }
 
     async post(req, res) {
-        if (req.headers.authorization !== config.apis.dbl) return res.end(403);
+        if (req.headers.authorization !== config.apis.dbl) {
+            res.statusCode = 401;
+            return res.end(JSON.stringify({ message: "Unauthenticated" }));
+        }
+
         const data = req.body;
 
         const user = await this.client.users.fetch(data.user).catch(() => null);
-        if (!user) return res.end(404);
+        if (!user) {
+            res.statusCode = 404;
+            return res.end(JSON.stringify({ message: "User not found" }));
+        }
 
         await user.settings.sync(true);
         let amount = this.client.funcs.randomNumber(100, 200);
@@ -22,7 +29,8 @@ module.exports = class extends Route {
         if (user && user.send) user.send(`<:penguSuccess:435712876506775553> ***Thank you for Upvoting PenguBot on DiscordBots.org, you've recieved ❄ \`${amount}\` Snowflakes as a bonus! You can do this again after 12 hours at <https://discordbots.org/bot/PenguBot/vote>. You can use these Snowflakes to play games, give them to other people, buy profile backgrounds or save up for upcoming features!***\n\n**Tip: **\`Voting on Weekends will give you double reward than usual so don't forget to upvote then as well!\``).catch(() => null);
         await user.settings.update([["snowflakes", user.settings.snowflakes + amount], ["lastUpvote", Date.now()]]);
 
-        return res.end();
+        res.statusCode = 200;
+        return res.end(JSON.stringify({ message: "Successful" }));
     }
 
 };
