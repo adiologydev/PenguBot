b1514e59a26119ef98ad654940048289d92542ca
Anish Shobith <anish.shobith19@gmail.com>
2019-08-03 16:22:00 +0530

    feat: add invite.gg to inviteRegex

--- src/monitors/antiinvites.js
+++ src/monitors/antiinvites.js
@@ -1,5 +1,5 @@
 const { Monitor, ServerLog } = require("../index");
-const inviteRegex = /(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite)\/.+/;
+const inviteRegex = /(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite|inivte\.gg)\/.+/;
 
 module.exports = class extends Monitor {
 
