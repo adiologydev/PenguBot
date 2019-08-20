1fac0bbccfa8c180d1536553729055b7d80c93b3
Jacz <itsmejacz@gmail.com>
2019-08-20 10:28:57 +1000

    Fix osu for now

--- src/commands/Game Statistics/osu.js
+++ src/commands/Game Statistics/osu.js
@@ -1,5 +1,4 @@
-const Command = require("../../lib/structures/KlasaCommand");
-
+const { Command, config } = require("../../index");
 module.exports = class extends Command {
 
     constructor(...args) {
@@ -9,12 +8,12 @@ module.exports = class extends Command {
             requiredPermissions: ["ATTACH_FILES", "USE_EXTERNAL_EMOJIS", "EMBED_LINKS"],
             description: language => language.get("COMMAND_OSU_DESCRIPTION"),
             extendedHelp: "No extended help available.",
-            usage: "<Username:str>"
+            usage: "<username:str>"
         });
     }
 
-    async run(msg, [Username]) {
-        const image = await this.client.idiotic.osu(Username).catch(() => null);
+    async run(msg, [username]) {
+        const image = await this.fetchURL("https://dev.anidiots.guide/generators/osu", { type: "buffer", query: { user: username, theme: "dark" }, headers: { Authorization: config.apis.idiotic } });
         if (!image) return msg.reply(msg.language.get("CMD_OSU_ERR"));
         return msg.channel.sendFile(image);
     }
