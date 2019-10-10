1bf867aa88cc866b6ed00dadef87044052108569
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2019-10-10 23:39:19 +0530

    Revert "update: rollback yt changes to songname"
    This reverts commit 1038439f4ec4104cb5893456198f7630df84f266.

--- src/arguments/songname.js
+++ src/arguments/songname.js
@@ -1,6 +1,7 @@
 const { Argument, Song, util: { showSeconds }, config } = require("../index");
 
 const wildcard = /(?:scsearch:|ytsearch:).*/i;
+const yt = /^(http(s)?:\/\/)?((w){3}.|music.)?youtu(be|.be)?(\.com)?\/.+/;
 const paste = /https:\/\/paste.pengubot.com\/(.*)/i;
 const spotifyList = /https?:\/\/(?:embed\.|open\.)(?:spotify\.com\/)(?:playlist\/|user\/spotify\/playlist\/|\?uri=spotify:playlist:)([1-z]{22})/i;
 const spotifyAlbum = /https?:\/\/(?:embed\.|open\.)(?:spotify\.com\/)(?:album\/|\?uri=spotify:album:)((\w|-){22})/i;
@@ -18,6 +19,7 @@ module.exports = class extends Argument {
 
         const validLink = this.isLink(arg);
         if (validLink) {
+            if (yt.test(arg)) throw `> ${this.client.emotes.cross} Due to recent change in YouTube's API, all Discord Bots and similar services are unable to play any YouTube videos/streams. We'll be sure to update everyone on further information soon. Thanks for using PenguBot.`;
             const result = await this.validLinkSearch(msg, arg);
             if (result.tracks.length) {
                 results.push(...result.tracks);
@@ -49,8 +51,7 @@ module.exports = class extends Argument {
     }
 
     async searchTrack(msg, arg) {
-        let data = await this.fetchTracks(`ytsearch:${arg}`);
-        if (!data || !data.tracks.length) data = await this.fetchTracks(`scsearch:${arg}`);
+        const data = await this.fetchTracks(`scsearch:${arg}`);
         if (!data || !data.tracks.length) throw msg.language.get("ER_MUSIC_NF");
 
         const songs = data.tracks.slice(0, 5);
@@ -85,7 +86,7 @@ ${msg.author}, Please select a track by replying from range \`1-5\` to add it to
         const tracks = [];
 
         for (const { track } of data.tracks.items) {
-            const searchResult = await this.fetchTracks(`ytsearch:${track.album.artists[0].name || track.artists[0].name} ${track.name} audio`);
+            const searchResult = await this.fetchTracks(`scsearch:${track.album.artists[0].name || track.artists[0].name} ${track.name} audio`);
             if (!searchResult.tracks.length) continue;
             tracks.push(searchResult.tracks[0]);
         }
@@ -101,7 +102,7 @@ ${msg.author}, Please select a track by replying from range \`1-5\` to add it to
 
         const [artist] = data.artists;
 
-        const searchResult = await this.fetchTracks(`ytsearch:${artist ? artist.name : ""} ${data.name} audio`);
+        const searchResult = await this.fetchTracks(`scsearch:${artist ? artist.name : ""} ${data.name} audio`);
         if (!searchResult.tracks.length) throw msg.language.get("ER_MUSIC_NF");
 
         return { tracks: [searchResult.tracks[0]] };
@@ -116,7 +117,7 @@ ${msg.author}, Please select a track by replying from range \`1-5\` to add it to
         const tracks = [];
 
         for (const track of data.tracks.items) {
-            const searchResult = await this.fetchTracks(`ytsearch:${track.artists[0].name} ${track.name} audio`);
+            const searchResult = await this.fetchTracks(`scsearch:${track.artists[0].name} ${track.name} audio`);
             if (!searchResult.tracks.length) continue;
             tracks.push(searchResult.tracks[0]);
         }
