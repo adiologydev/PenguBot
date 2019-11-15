d4e9ca5951a4243bd9ed8448455022d480f68b68
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2019-11-15 19:15:28 +0530

    add: debug event

---
+++ src/events/debug.js
@@ -0,0 +1,19 @@
+const { Event, config } = require("../index");
+
+module.exports = class extends Event {
+
+    constructor(...args) {
+        /**
+         * Any default options can be omitted completely.
+         * if all options are default, you can omit the constructor completely
+         */
+        super(...args, {
+            enabled: config.debug || false
+        });
+    }
+
+    async run(info) {
+        console.log(`[DEBUG] ${info}`);
+    }
+
+};
