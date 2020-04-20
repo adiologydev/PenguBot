772d2c01e65a6c5b56cd586a456151dbc76dd3d2
Jacz <itsmejacz@gmail.com>
2020-04-20 15:07:21 +1000

    Remove my comments

--- src/events/debug.js
+++ src/events/debug.js
@@ -3,10 +3,6 @@ const { Event, config } = require("../index");
 module.exports = class extends Event {
 
     constructor(...args) {
-        /**
-         * Any default options can be omitted completely.
-         * if all options are default, you can omit the constructor completely
-         */
         super(...args, {
             enabled: "debug" in config ? config.debug : false
         });
