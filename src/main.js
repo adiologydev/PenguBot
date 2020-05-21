f4be327180d823b441c22894d880021fb312f8a5
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-05-21 22:37:26 +0530

    add: spotify support back

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ const sharder = new ShardingManager(join(__dirname, "PenguBot"), {
         messageCacheLifetime: 120,
         commandMessageLifetime: 120,
         owners: ["136549806079344640", "152386257568137216"],
-        music: { nodes: config.nodes, lyrics: config.apis.lyrics }
+        music: { nodes: config.nodes, lyrics: config.apis.lyrics, spotify: { buffer: config.apis.spotify, token: "" } }
     },
     shardCount: config.shards,
     ipcSocket: config.patreon ? 9545 : 9454,
