028c4c102f474e2e8de5f258f42588d8b05d4c62
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-06-07 16:35:26 +0530

    fix(cmd): osu

--- src/commands/Game Statistics/osu.js
+++ src/commands/Game Statistics/osu.js
@@ -1,5 +1,5 @@
-const { Command, config, MessageEmbed, Timestamp } = require("../../index");
-const timestamp = new Timestamp("HH[h] MM[n] SS[s]");
+const { Command, config, MessageEmbed, Duration } = require("../../index");
+
 module.exports = class extends Command {
 
     constructor(...args) {
@@ -14,25 +14,26 @@ module.exports = class extends Command {
     }
 
     async run(msg, [username]) {
-        const data = await this.fetchURL(`https://osu.ppy.sh/api/get_user`, { type: "json", query: { k: config.apis.osu, u: encodeURIComponent(username) } });
+        const data = await this.fetchURL(`https://osu.ppy.sh/api/get_user`, { type: "json", query: { k: config.apis.osu, u: encodeURIComponent(username) } }).then(a => a[0]).catch(() => null);
         if (!data) return msg.reply(msg.language.get("CMD_OSU_ERR"));
         const embed = new MessageEmbed()
-            .setImage(`https://a.ppy.sh/${data.user_id}`)
+            .setThumbnail(`https://a.ppy.sh/${data.user_id}`)
             .setColor("#EF5E9F")
             .setAuthor(data.username, `https://a.ppy.sh/${data.user_id}`, "https://pengubot.com")
             .addField("Total Score", Number(data.total_score).toLocaleString(), true)
             .addField("Ranked Score", Number(data.ranked_score).toLocaleString(), true)
-            .addField("Level", Number(data.level).toLocaleString(), true)
-            .addField("Score 50", Number(data.score50).toLocaleString(), true)
-            .addField("Score 100", Number(data.score100).toLocaleString(), true)
-            .addField("Score 300", Number(data.score300).toLocaleString(), true)
-            .addField("PP Rank", Number(data.pp_rank).toLocaleString(), true)
+            .addField("Level", Number(data.level).toFixed(0).toLocaleString(), true)
+            .addField("Count 50", Number(data.count50).toLocaleString(), true)
+            .addField("Count 100", Number(data.count100).toLocaleString(), true)
+            .addField("Count 300", Number(data.count300).toLocaleString(), true)
+            .addField("Global Rank", Number(data.pp_rank).toLocaleString(), true)
             .addField("SS Rank", Number(data.count_rank_ss).toLocaleString(), true)
             .addField("SSH Rank", Number(data.count_rank_ssh).toLocaleString(), true)
             .addField("S Rank", Number(data.count_rank_s).toLocaleString(), true)
             .addField("A Rank", Number(data.count_rank_a).toLocaleString(), true)
-            .addField("Time Played", timestamp.display(new Date(data.total_seconds_played * 1000)), true)
-            .addField("Accuracy", Number(data.accuracy).toFixed(2), true);
+            .addField("Accuracy", `${Number(data.accuracy).toFixed(2)}%`, true)
+            .addField("Time Played", Duration.toNow(Date.now() - (data.total_seconds_played * 1000)), true)
+            .setFooter("© PenguBot.com");
         return msg.channel.send({ embed });
     }
 
